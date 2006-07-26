ca74fdeecfa63356aa3fbc10dd378608e49850bd
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2006-07-26 07:31:26 +0000

    Changed keywords.

--- src/org/jvoicexml/ApplicationRegistry.java
+++ src/org/jvoicexml/ApplicationRegistry.java
@@ -1,8 +1,8 @@
 /*
  * File:    $RCSfile: ApplicationRegistry.java,v $
- * Version: $Revision: 1.1 $
- * Date:    $Date: 2006/03/09 04:56:57 $
- * Author:  $Author: schnelle $
+ * Version: $Revision$
+ * Date:    $Date$
+ * Author:  $Author$
  * State:   $State: Exp $
  *
  * JVoiceXML - A free VoiceXML implementation.
@@ -43,7 +43,7 @@ import java.net.URI;
  * @see Application
  *
  * @author Dirk Schnelle
- * @version $Revision: 1.1 $
+ * @version $Revision$
  *
  * <p>
  * Copyright &copy; 2006 JVoiceXML group - <a
--- src/org/jvoicexml/application/JVoiceXmlApplicationRegistry.java
+++ src/org/jvoicexml/application/JVoiceXmlApplicationRegistry.java
@@ -1,8 +1,8 @@
 /*
- * File:    $HeadURL: https://svn.sourceforge.net/svnroot/jvoicexml/trunk/src/org/jvoicexml/Application.java $
- * Version: $LastChangedRevision: 23 $
+ * File:    $HeadURL$
+ * Version: $LastChangedRevision$
  * Date:    $LastChangedDate: $
- * Author:  $LastChangedBy: schnelle $
+ * Author:  $LastChangedBy$
  *
  * JVoiceXML - A free VoiceXML implementation.
  *
@@ -39,7 +39,7 @@ import org.jvoicexml.ApplicationRegistry;
  * @see org.jvoicexml.ApplicationRegistry
  *
  * @author Dirk Schnelle
- * @version $LastChangedRevision: 23 $
+ * @version $LastChangedRevision$
  *
  * <p>
  * Copyright &copy; 2005 -2006JVoiceXML group - <a
