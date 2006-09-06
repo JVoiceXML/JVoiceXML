e82dd928e8f24eeb32f1920009edc140921ffa86
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2006-09-06 11:15:30 +0000

    added buffer for messages.

--- trunk/src/org/jvoicexml/implementation/client/RemoteAudioSystem.java
+++ trunk/src/org/jvoicexml/implementation/client/RemoteAudioSystem.java
@@ -36,13 +36,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.Semaphore;
 
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.DataLine;
-import javax.sound.sampled.SourceDataLine;
-
 import org.jvoicexml.implementation.CallControl;
-import javax.sound.sampled.*;
 
 /**
  * Audio system to be used remotely by the VoiceXML interpreter.
@@ -66,17 +60,17 @@ public final class RemoteAudioSystem
     /** The address of the server socket. */
     private InetAddress address;
 
-    /** The used line. */
-    private SourceDataLine line;
-
-    /** The used audio format. */
-    private AudioFormat format;
-
     /**
      * Sempaphore to control the sync the start of the server with the retrieval
      * of the {@link CallControl}.
      */
     private final Semaphore sem;
+
+    /** The output thread. */
+    private AudioOutputThread outputThread;
+
+    /** The output stream. */
+    private ObjectOutputStream out;
 
     /**
      * Constructs a new object.
@@ -84,18 +78,6 @@ public final class RemoteAudioSystem
      */
     public RemoteAudioSystem(final int prt) {
         port = prt;
-
-        format = new AudioFormat(16000, 16, 1, true, true);
-        final DataLine.Info dataLineInfo =
-                new DataLine.Info(SourceDataLine.class, format);
-
-        try {
-            line = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
-        } catch (javax.sound.sampled.LineUnavailableException lue) {
-            lue.printStackTrace();
-
-            line = null;
-        }
 
         sem = new Semaphore(1);
         try {
@@ -111,47 +93,38 @@ public final class RemoteAudioSystem
      * Starts this audio system.
      */
     public void run() {
-        if (line == null) {
-            return;
-        }
-
         try {
             final ServerSocket server = new ServerSocket(port);
             address = InetAddress.getLocalHost();
+
+            outputThread = new AudioOutputThread(this);
+            outputThread.start();
 
             sem.release();
 
             final Socket client = server.accept();
             final InputStream in = client.getInputStream();
-            final OutputStream out = client.getOutputStream();
-
-            final int length = 4096;
-            line.open(format, length);
-            line.start();
+            final OutputStream outStream = client.getOutputStream();
 
             final ObjectInputStream input = new ObjectInputStream(in);
-            final ObjectOutputStream output = new ObjectOutputStream(out);
-            communicate(input, output);
+            out = new ObjectOutputStream(outStream);
+            communicate(input);
             input.close();
 
             client.close();
             server.close();
         } catch (IOException ioe) {
             ioe.printStackTrace();
-        } catch (javax.sound.sampled.LineUnavailableException lue) {
-            lue.printStackTrace();
         }
     }
 
     /**
      * Communicate with the VoiceXML interpreter.
      * @param in Input stream from the server.
-     * @param out Output stream to the server.
      * @throws IOException
      *         Error accessing the stream.
      */
-    private void communicate(final ObjectInputStream in,
-                             final ObjectOutputStream out)
+    private void communicate(final ObjectInputStream in)
             throws IOException {
         do {
             final Object object;
@@ -163,42 +136,20 @@ public final class RemoteAudioSystem
                 return;
             }
 
-            if (object instanceof AudioMessage) {
-                final AudioMessage msg = (AudioMessage) object;
-                playAudio(msg);
-            } else if (object instanceof AudioStartMessage) {
-                // Ignore the start message.
-            } else if (object instanceof MarkerMessage) {
-                line.drain();
-                out.writeObject(object);
-            } else if (object instanceof AudioFormatMessage) {
-                final AudioFormatMessage msg = (AudioFormatMessage) object;
-                final AudioFormat format = msg.getAudioFormat();
-                line.drain();
-                line.stop();
-                line.close();
-                try {
-                    line.open(format, 4096);
-                } catch (LineUnavailableException ex) {
-                    ex.printStackTrace();
-                }
-                line.start();
-            } else if (object instanceof AudioEndMessage) {
-                line.drain();
-                out.writeObject(object);
-            } else {
-                System.err.println("cannot handle object " + object);
-            }
+            outputThread.addMessage(object);
         } while (true);
     }
 
     /**
-     * Plays the received audio in the local audio system.
-     * @param message The audio data to play.
+     * Sends a message back to the VoiceXML interpreter.
+     * @param message The message to send.
      */
-    private void playAudio(final AudioMessage message) {
-        byte[] buffer = message.getBuffer();
-        line.write(buffer, 0, buffer.length);
+    void sendMessage(final Object message) {
+        try {
+            out.writeObject(message);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
     /**
