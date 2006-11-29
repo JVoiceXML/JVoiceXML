9a4a397f6cc542001c438d75461873d593030d81
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2006-11-29 08:40:06 +0000

    Security commit.

--- trunk/src/org/jvoicexml/event/plain/jvxml/RecognitionEvent.java
+++ trunk/src/org/jvoicexml/event/plain/jvxml/RecognitionEvent.java
@@ -1,8 +1,8 @@
 /*
  * File:    $RCSfile: RecognitionEvent.java,v $
- * Version: $Revision: 1.8 $
- * Date:    $Date: 2006/04/28 07:28:45 $
- * Author:  $Author: schnelle $
+ * Version: $Revision$
+ * Date:    $Date$
+ * Author:  $Author$
  * State:   $State: Exp $
  *
  * JVoiceXML - A free VoiceXML implementation.
@@ -34,7 +34,7 @@ import org.jvoicexml.implementation.RecognitionResult;
  * The user has responded within the timeout interval.
  *
  * @author Dirk Schnelle
- * @version $Revision: 1.8 $
+ * @version $Revision$
  *
  * <p>
  * Copyright &copy; 2005-2006 JVoiceXML group -
--- trunk/src/org/jvoicexml/event/plain/jvxml/ReturnEvent.java
+++ trunk/src/org/jvoicexml/event/plain/jvxml/ReturnEvent.java
@@ -1,8 +1,8 @@
 /*
  * File:    $RCSfile: ReturnEvent.java,v $
- * Version: $Revision: 1.4 $
- * Date:    $Date: 2006/04/25 10:56:02 $
- * Author:  $Author: schnelle $
+ * Version: $Revision$
+ * Date:    $Date$
+ * Author:  $Author$
  * State:   $State: Exp $
  *
  * JVoiceXML - A free VoiceXML implementation.
@@ -35,7 +35,7 @@ import org.jvoicexml.event.plain.PlainEvent;
  * The FIA processed a <code>&lt;return&gt;</code> event.
  *
  * @author Dirk Schnelle
- * @version $Revision: 1.4 $
+ * @version $Revision$
  *
  * @since 0.3
  *
