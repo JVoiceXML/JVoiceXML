2f48437c65dfcf6c3eb91e0533fc9eddb0470a3f
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2006-11-29 08:31:39 +0000

    Security commit.

--- trunk/src/org/jvoicexml/event/GenericVoiceXmlEvent.java
+++ trunk/src/org/jvoicexml/event/GenericVoiceXmlEvent.java
@@ -1,8 +1,8 @@
 /*
  * File:    $RCSfile: GenericVoiceXmlEvent.java,v $
- * Version: $Revision: 1.4 $
- * Date:    $Date: 2005/12/13 08:28:24 $
- * Author:  $Author: schnelle $
+ * Version: $Revision$
+ * Date:    $Date$
+ * Author:  $Author$
  * State:   $State: Exp $
  *
  * JVoiceXML - A free VoiceXML implementation.
@@ -32,7 +32,7 @@ package org.jvoicexml.event;
  * <code>&lt;throw&gt;</code> tag.
  *
  * @author Dirk Schnelle
- * @version $Revision: 1.4 $
+ * @version $Revision$
  *
  * <p>
  * Copyright &copy; 2005 JVoiceXML group -
