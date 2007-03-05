0c985d0a06ebde73044d0e24785af0cd739ac3fd
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2007-03-05 18:24:55 +0000

    Started refactoring of application.

--- test/unitTests/org/jvoicexml/interpreter/TestJVoiceXmlApplication.java
+++ test/unitTests/org/jvoicexml/interpreter/TestJVoiceXmlApplication.java
@@ -29,9 +29,14 @@ package org.jvoicexml.interpreter;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import javax.xml.parsers.ParserConfigurationException;
+
 import junit.framework.TestCase;
 
 import org.jvoicexml.Application;
+import org.jvoicexml.event.error.BadFetchError;
+import org.jvoicexml.xml.vxml.VoiceXmlDocument;
+import org.jvoicexml.xml.vxml.Vxml;
 
 /**
  * Test case for org.jvoicexml.interpreter.JVoiceXmlApplication.
@@ -55,42 +60,145 @@ public final class TestJVoiceXmlApplication
      *
      * @param scheme
      *        Scheme name.
-     * @param ssp
-     *        Specific part.
+     * @param host
+     *        Host
+     * @param path
+     *        Path
      * @param fragment
      *        Fragment.
-     * @return Create URI, <code>null</code> if we trapped inot an exception.
+     * @return Create URI, <code>null</code> if we trapped into an exception.
      */
-    private URI createUri(final String scheme, final String ssp,
-                          final String fragment) {
+    private URI createUri(final String scheme, final String host,
+                final String path, final String fragment) {
         try {
-            return new URI(scheme, ssp, fragment);
+            return new URI(scheme, host, path, fragment);
         } catch (URISyntaxException use) {
             fail("could not create URI " + use.toString());
         }
 
         return null;
     }
+
     /**
      * Test method for
-     * 'JVoiceXmlApplication.getUri()'.
+     * 'JVoiceXmlApplication.addDocument()'.
      *
-     * @see JVoiceXmlApplication#getCurrentUri()
+     * @see JVoiceXmlApplication#addDocument(org.jvoicexml.xml.vxml.VoiceXmlDocument)
      */
-    public void testGetCurrentUri() {
-        final String testApplication1 = "test1";
-        final URI testUri1 = createUri("scheme", "ssp", "fragment");
+    public void testAddDocument() {
+        final Application application = new JVoiceXmlApplication();
 
-        final Application application1 =
-                new JVoiceXmlApplication(testUri1);
+        VoiceXmlDocument doc1 = null;
+        try {
+            doc1 = new VoiceXmlDocument();
+        } catch (ParserConfigurationException e) {
+            fail(e.getMessage());
+        }
+        final Vxml vxml1 = doc1.getVxml();
+        final URI testUri1 = createUri("scheme", "host", "/path", "fragment");
 
-        assertEquals(testUri1, application1.getCurrentUri());
+        vxml1.setXmlBase(testUri1);
 
-        final URI testUri2 = null;
+        try {
+            application.addDocument(doc1);
+        } catch (BadFetchError e) {
+            fail(e.getMessage());
+        }
 
-        final Application application2 =
-                new JVoiceXmlApplication(testUri2);
+        assertEquals(testUri1, application.getApplication());
 
-        assertNull(application2.getCurrentUri());
+        VoiceXmlDocument doc2 = null;
+        try {
+            doc2 = new VoiceXmlDocument();
+        } catch (ParserConfigurationException e) {
+            fail(e.getMessage());
+        }
+        final Vxml vxml2 = doc2.getVxml();
+        final URI testUri2 = createUri("scheme", "host", "/path", "fragment");
+        vxml2.setXmlBase(testUri2);
+
+        try {
+            application.addDocument(doc2);
+        } catch (BadFetchError e) {
+            fail(e.getMessage());
+        }
+
+        assertEquals(testUri1, application.getApplication());
+
+        VoiceXmlDocument doc3 = null;
+        try {
+            doc3 = new VoiceXmlDocument();
+        } catch (ParserConfigurationException e) {
+            fail(e.getMessage());
+        }
+        final Vxml vxml3 = doc3.getVxml();
+        final URI testUri3 =
+            createUri("scheme3", "host3", "/path3", "fragment3");
+        vxml3.setXmlBase(testUri3);
+
+        try {
+            application.addDocument(doc3);
+        } catch (BadFetchError e) {
+            fail(e.getMessage());
+        }
+
+        assertEquals(testUri3, application.getApplication());
+    }
+
+    /**
+     * Test method for
+     * 'JVoiceXmlApplication.resolve()'.
+     *
+     * @see JVoiceXmlApplication#resolve(URI)
+     */
+    public void testResolve() {
+        final Application application = new JVoiceXmlApplication();
+        VoiceXmlDocument doc1 = null;
+        try {
+            doc1 = new VoiceXmlDocument();
+        } catch (ParserConfigurationException e) {
+            fail(e.getMessage());
+        }
+        final Vxml vxml1 = doc1.getVxml();
+        final URI testUri1 =
+            createUri("scheme", "host", "/path/subpath1", "fragment");
+
+        vxml1.setXmlBase(testUri1);
+
+        try {
+            application.addDocument(doc1);
+        } catch (BadFetchError e) {
+            fail(e.getMessage());
+        }
+
+        final URI testUri2 =
+            createUri("scheme", "host", "/path/subpath2", "fragment");
+        assertEquals(testUri2, application.resolve(testUri2));
+
+        final URI testUri3 =
+            createUri("scheme", "host", "/path/subpath3", null);
+        URI testUri3Relative = null;
+        try {
+            testUri3Relative = new URI("subpath3");
+        } catch (URISyntaxException e) {
+            fail(e.getMessage());
+        }
+        assertEquals(testUri3, application.resolve(testUri3Relative));
+
+        final URI testUri4 =
+            createUri("scheme", "host", "/path/subpath3/extendedsubpath", null);
+        URI testUri4Relative = null;
+        try {
+            testUri4Relative = new URI("subpath3/extendedsubpath");
+        } catch (URISyntaxException e) {
+            fail(e.getMessage());
+        }
+        assertEquals(testUri4, application.resolve(testUri4Relative));
+
+        final URI testUri5 =
+            createUri("scheme2", "host", "/path/subpath3", null);
+        assertEquals(testUri5, application.resolve(testUri5));
+
+        assertNull(application.resolve(null));
     }
 }
