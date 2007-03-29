ff61d8fc9f0e4f8b72f16b462046bc0f3f1c38b0
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2007-03-29 07:46:05 +0000

    Introduced resource factory for audio file output.

---
+++ src/org/jvoicexml/implementation/jsapi10/jvxml/AudioFileOutputFactory.java
@@ -0,0 +1,100 @@
+/*
+ * File:    $HeadURL: https://jvoicexml.svn.sourceforge.net/svnroot/jvoicexml/trunk/src/org/jvoicexml/implementation/jsapi10/jvxml/SystemOutputFactory.java $
+ * Version: $LastChangedRevision: 172 $
+ * Date:    $LastChangedDate: 2006-12-14 09:35:30 +0100 (Do, 14 Dez 2006) $
+ * Author:  $LastChangedBy: schnelle $
+ *
+ * JVoiceXML - A free VoiceXML implementation.
+ *
+ * Copyright (C) 2006-2007 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package org.jvoicexml.implementation.jsapi10.jvxml;
+
+import org.jvoicexml.AudioFileOutput;
+import org.jvoicexml.event.error.NoresourceError;
+import org.jvoicexml.implementation.ResourceFactory;
+import org.jvoicexml.implementation.jsapi10.Jsapi10AudioFileOutput;
+import org.jvoicexml.logging.Logger;
+import org.jvoicexml.logging.LoggerFactory;
+
+/**
+ * Demo implementation of a
+ * {@link org.jvoicexml.implementation.ResourceFactory} for the
+ * {@link AudioFileOutput} based on JSAPI 1.0.
+ *
+ * @author Dirk Schnelle
+ * @version $Revision: 172 $
+ *
+ * <p>
+ * Copyright &copy; 2006-2007 JVoiceXML group -
+ * <a href="http://jvoicexml.sourceforge.net">
+ * http://jvoicexml.sourceforge.net/</a>
+ * </p>
+ *
+ * @since 0.5.5
+ */
+public final class AudioFileOutputFactory
+implements ResourceFactory<AudioFileOutput> {
+    /** Logger for this class. */
+    private static final Logger LOGGER =
+        LoggerFactory.getLogger(AudioFileOutputFactory.class);
+
+    /** Number of instances that this factory will create. */
+    private int instances;
+
+    /** Name of the default voice. */
+    private String voice;
+
+    /**
+     * Constructs a new object.
+     */
+    public AudioFileOutputFactory() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public AudioFileOutput createResource()
+        throws NoresourceError {
+
+        return new Jsapi10AudioFileOutput();
+    }
+
+    /**
+     * Sets the number of instances that this factory will create.
+     * @param number Number of instances to create.
+     */
+    public void setInstances(final int number) {
+        instances = number;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int getInstances() {
+        return instances;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String getType() {
+        return "jsapi10";
+    }
+}
