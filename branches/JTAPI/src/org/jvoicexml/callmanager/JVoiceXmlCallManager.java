10dcdd5f0a7f8b950e65f321f143f87f888ffa8a
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2007-04-11 11:28:48 +0000

    Renaming of classe.

---
+++ branches/JTAPI/src/org/jvoicexml/callmanager/JVoiceXmlCallManager.java
@@ -0,0 +1,171 @@
+package org.jvoicexml.callmanager;
+
+import java.net.URI;
+import javax.telephony.JtapiPeer;
+import javax.telephony.JtapiPeerFactory;
+import javax.telephony.JtapiPeerUnavailableException;
+import javax.telephony.ProviderUnavailableException;
+import javax.telephony.Provider;
+import javax.telephony.Address;
+import net.sourceforge.gjtapi.test.TestIncomingCallListenerObserver;
+import java.util.HashMap;
+import javax.telephony.*;
+import javax.telephony.*;
+import net.sourceforge.gjtapi.media.GenericMediaService;
+import javax.telephony.media.MediaProvider;
+import javax.telephony.media.MediaConfigException;
+import javax.telephony.media.MediaBindException;
+import net.sourceforge.gjtapi.raw.sipprovider.common.Console;
+
+/**
+ * <p>Title: Call Manager</p>
+ *
+ * <p>Description: </p>
+ *
+ * <p>Copyright: Copyright (c) 2007</p>
+ *
+ * <p>Company: L2f,Inesc-id</p>
+ *
+ * @author
+ * @version 1.0
+ */
+public class JVoiceXmlCallManager implements CallManager {
+
+    //log
+    protected static Console console = Console.getConsole(JVoiceXmlCallManager.class);
+
+    //peer
+    protected static String JtapiPeer =
+            "net.sourceforge.gjtapi.GenericJtapiPeer";
+
+    //provider
+    private static Provider _provider = null;
+
+    //name of the provider used ex:net.sourceforge.gjtapi.raw.sipprovider.SipProvider
+    private static String _providerName = null;
+
+    //Map of terminals associated to an Application
+    HashMap<String, URI> _associateTerminalToVxml = new HashMap<String, URI>();
+
+    /**
+     * Provider initialization and properties for the terminals
+     */
+    public JVoiceXmlCallManager() {
+        console.logEntry();
+
+        // Get a JTAPI Peer
+        JtapiPeer peer = null;
+        try {
+            peer = JtapiPeerFactory.getJtapiPeer(JtapiPeer);
+            console.debug("1.1: Successfully loaded the JTapi Peer");
+        } catch (JtapiPeerUnavailableException jpue) {
+            console.debug("1.1: Failed to locate Peer with the factory");
+            jpue.printStackTrace();
+        }
+
+        // initialize and load properties
+        try {
+            _provider = peer.getProvider(_providerName);
+            console.debug("1.2: Successfully loaded Provider");
+        } catch (ProviderUnavailableException pue) {
+            console.debug("1.2: Failed to load Provider");
+            pue.printStackTrace();
+        }
+        console.logExit();
+    }
+
+    public static void main(String[] args) {
+        console.logEntry();
+        if (args.length < 1) {
+            System.err.println(
+                    "Usage: java net.sourceforge.gjtapi.test.CallManagerIMP <Provider> ");
+            //"net.sourceforge.gjtapi.raw.sipprovider.SipProvider";
+            System.exit(1);
+        }
+
+        _providerName = args[0];
+        JVoiceXmlCallManager _callManager = null;
+        _callManager = new JVoiceXmlCallManager();
+        _callManager.start();
+        console.logExit();
+    }
+
+    /**
+     * start to listening, i.e., associate a Listener and an observer to
+     * each terminal of the provider
+     */
+    public void start() {
+        console.logEntry();
+        try {
+            Address[] address = _provider.getAddresses();
+
+            for (int i = 0; i < address.length; i++) {
+
+                //address
+                String addr = address[i].getName();
+                console.debug("address name : " + addr);
+                //terminal
+                Terminal terminal = _provider.getTerminal(addr);
+                console.debug("terminal name: " + terminal.getName());
+
+                //Create a media service
+                console.debug("Attempting to create a Media Service...");
+                GenericMediaService ms = new GenericMediaService((MediaProvider)
+                        _provider);
+
+                //we have only one terminal per Address
+                ms.bindToTerminal(null, terminal);
+
+                JVoiceXmlCallControl callControl = new JVoiceXmlCallControl(ms);
+
+            }
+        } catch (ResourceUnavailableException ex) {
+            console.error("", ex);
+            ex.printStackTrace();
+        } catch (MediaConfigException ex) {
+            console.error("", ex);
+            ex.printStackTrace();
+        } catch (MediaBindException ex) {
+            ex.printStackTrace();
+            console.error("", ex);
+        } catch (javax.telephony.InvalidArgumentException ex) {
+            console.error("", ex);
+            ex.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Stop the provider
+     */
+    public void stop() {
+        console.logEntry();
+        /**
+         * @todo may be it is necessary to stop all the listerners
+         */
+
+        console.debug("ShutingDown the provider");
+        _provider.shutdown();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+            console.error("", ex);
+        }
+        console.logExit();
+    }
+
+    /**
+     * Add a given application (URI) to a terminal
+     * @param application URI
+     * @param terminal String
+     * @return boolean
+     */
+    public boolean addTerminal(URI application, String terminal) {
+        console.logEntry();
+        _associateTerminalToVxml.put(terminal, application);
+        console.logExit();
+        return true;
+    }
+
+}
