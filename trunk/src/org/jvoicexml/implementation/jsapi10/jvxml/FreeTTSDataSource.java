c2476240f8db431e8146def3bee20f7c6ae089f7
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2007-08-10 09:24:06 +0000

    Initial release.

---
+++ trunk/src/org/jvoicexml/implementation/jsapi10/jvxml/FreeTTSDataSource.java
@@ -0,0 +1,129 @@
+/*
+ * File:    $HeadURL: https://jvoicexml.svn.sourceforge.net/svnroot/jvoicexml/trunk/src/org/jvoicexml/implementation/jsapi10/jvxml/SystemOutputFactory.java $
+ * Version: $LastChangedRevision: 172 $
+ * Date:    $LastChangedDate: 2006-12-14 09:35:30 +0100 (Do, 14 Dez 2006) $
+ * Author:  $LastChangedBy: schnelle $
+ *
+ * JVoiceXML - A free VoiceXML implementation.
+ *
+ * Copyright (C) 2007 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package org.jvoicexml.implementation.jsapi10.jvxml;
+
+import java.io.IOException;
+
+import javax.media.Time;
+import javax.media.protocol.PullDataSource;
+import javax.media.protocol.PullSourceStream;
+
+/**
+ * A {@link javax.media.protocol.DataSource} for FreeTTS.
+ *
+ * @author Dirk Schnelle
+ * @version $Revision: 172 $
+ *
+ * <p>
+ * Copyright &copy; 2007 JVoiceXML group -
+ * <a href="http://jvoicexml.sourceforge.net">
+ * http://jvoicexml.sourceforge.net/</a>
+ * </p>
+ *
+ * @since 0.6
+ */
+public final class FreeTTSDataSource extends PullDataSource {
+    /** The one and only source stream. */
+    private FreeTTSPullSourceStream stream;
+
+    /**
+     * Constructs a new object.
+     */
+    public FreeTTSDataSource() {
+        stream = new FreeTTSPullSourceStream();
+    }
+
+    /**
+     * @return the completed
+     */
+    public boolean isCompleted() {
+        return stream.endOfStream();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public PullSourceStream[] getStreams() {
+        PullSourceStream[] streams = new PullSourceStream[1];
+
+        streams[0] = stream;
+
+        return streams;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void connect() throws IOException {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void disconnect() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String getContentType() {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Object getControl(final String arg0) {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Object[] getControls() {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Time getDuration() {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void start() throws IOException {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void stop() throws IOException {
+    }
+}
---
+++ trunk/src/org/jvoicexml/implementation/jsapi10/jvxml/RtpServer.java
@@ -0,0 +1,206 @@
+/*
+ * File:    $HeadURL: https://jvoicexml.svn.sourceforge.net/svnroot/jvoicexml/trunk/src/org/jvoicexml/implementation/jsapi10/jvxml/SystemOutputFactory.java $
+ * Version: $LastChangedRevision: 172 $
+ * Date:    $LastChangedDate: 2006-12-14 09:35:30 +0100 (Do, 14 Dez 2006) $
+ * Author:  $LastChangedBy: schnelle $
+ *
+ * JVoiceXML - A free VoiceXML implementation.
+ *
+ * Copyright (C) 2007 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package org.jvoicexml.implementation.jsapi10.jvxml;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+import javax.media.Manager;
+import javax.media.MediaException;
+import javax.media.Player;
+import javax.media.Processor;
+import javax.media.format.AudioFormat;
+import javax.media.protocol.ContentDescriptor;
+import javax.media.protocol.DataSource;
+import javax.media.rtp.RTPManager;
+import javax.media.rtp.SendStream;
+import javax.media.rtp.SessionAddress;
+import javax.media.rtp.SessionManagerException;
+
+/**
+ * A general purpose RTP server based on JMF.
+ *
+ * @author Dirk Schnelle
+ * @version $Revision: 172 $
+ *
+ * <p>
+ * Copyright &copy; 2007 JVoiceXML group -
+ * <a href="http://jvoicexml.sourceforge.net">
+ * http://jvoicexml.sourceforge.net/</a>
+ * </p>
+ *
+ * @since 0.6
+ */
+public final class RtpServer {
+    /** Audio format. */
+    public static final AudioFormat FORMAT_ULAR_RTP = new AudioFormat(
+            AudioFormat.ULAW_RTP, 8000d, 8, 1, AudioFormat.LITTLE_ENDIAN,
+            AudioFormat.SIGNED);
+
+    /** The RTP manager. */
+    private RTPManager rtpManager;
+
+    /** The stream to send data. */
+    private SendStream sendStream;
+
+    /** The local IP address. */
+    private SessionAddress localAddress;
+
+    /**
+     * Constructs a new object taking a free random port and this computer
+     * as the local address.
+     * @throws IOException
+     *         Error creating the RTP manager.
+     * @throws SessionManagerException
+     *         Error creating the RTP manager.
+     * @throws MediaException
+     *         Error creating the RTP manager.
+     */
+    public RtpServer() throws IOException, SessionManagerException,
+            MediaException {
+        this(SessionAddress.ANY_PORT);
+    }
+
+    /**
+     * Constructs a new object.
+     * @param localPort local port number.
+     * @throws IOException
+     *         Error creating the RTP manager.
+     * @throws SessionManagerException
+     *         Error creating the RTP manager.
+     * @throws MediaException
+     *         Error creating the RTP manager.
+     */
+    public RtpServer(final int localPort) throws IOException,
+            SessionManagerException, MediaException {
+        rtpManager = RTPManager.newInstance();
+        InetAddress localIp = InetAddress.getLocalHost();
+        localAddress = new SessionAddress(localIp, localPort);
+        rtpManager.initialize(localAddress);
+    }
+
+    /**
+     * Adds a remote JMF player on this computer.
+     * @param remotePort port number of the JMF player.
+     * @throws IOException
+     *         Error resolving the local address.
+     * @throws SessionManagerException
+     *         Error adding the target.
+     */
+    public void addTarget(final int remotePort) throws IOException,
+            SessionManagerException {
+        InetAddress ipAddress = InetAddress.getLocalHost();
+        SessionAddress remoteAddress =
+            new SessionAddress(ipAddress, remotePort);
+        rtpManager.addTarget(remoteAddress);
+    }
+
+    /**
+     * Adds a remote JMF player on the specified remote computer.
+     * @param remoteHost name of the remote host.
+     * @param remotePort port number of the JMF player.
+     * @throws IOException
+     *         Error resolving the remote address.
+     * @throws SessionManagerException
+     *         Error adding the target.
+     */
+    public void addTarget(final String remoteHost, final int remotePort)
+            throws IOException, SessionManagerException {
+        InetAddress ipAddress = InetAddress.getByName(remoteHost);
+        SessionAddress remoteAddress =
+            new SessionAddress(ipAddress, remotePort);
+        rtpManager.addTarget(remoteAddress);
+    }
+
+    /**
+     * Initialize the send stream.
+     * @param sendStreamSource datasource to send data from.
+     * @throws IOException
+     *         Error initializing the stream.
+     * @throws MediaException
+     *         Error initializing the stream.
+     */
+    public void initSendStream(final DataSource sendStreamSource)
+        throws IOException, MediaException {
+        Processor proc = Manager.createProcessor(sendStreamSource);
+        proc.configure();
+        waitForState(proc, Processor.Configured);
+        proc.setContentDescriptor(new ContentDescriptor(
+                ContentDescriptor.RAW_RTP));
+        proc.start();
+        proc.getTrackControls()[0].setFormat(FORMAT_ULAR_RTP);
+        waitForState(proc, Player.Started);
+        sendStream = rtpManager.createSendStream(proc.getDataOutput(), 0);
+    }
+
+    /**
+     * Start sending.
+     * @throws IOException
+     *         Error starting the send stream.
+     */
+    public void startSending() throws IOException {
+        sendStream.start();
+    }
+
+    /**
+     * Stop sending.
+     * @throws IOException
+     *         Error stopping the send stream.
+     */
+    public void stopSending() throws IOException {
+        sendStream.stop();
+    }
+
+    /**
+     * Dispose.
+     */
+    public void dispose() {
+        rtpManager.removeTargets("Disconnected!");
+        rtpManager.dispose();
+    }
+
+    /**
+     * Delays until the processor reaches the specified state.
+     * @param processor the processor
+     * @param state the state to reach.
+     */
+    public static void waitForState(final Processor processor,
+            final int state) {
+        while (true) {
+            int actState = processor.getState();
+            if (state == actState) {
+                return;
+            }
+
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
