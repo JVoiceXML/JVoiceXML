8fdf0155ecb9febc015c1269a0236b1d953d7d89
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2007-10-24 07:51:38 +0000

    Added example on how to use the text based client.

--- trunk/src/org/jvoicexml/client/text/package-info.java
+++ trunk/src/org/jvoicexml/client/text/package-info.java
@@ -1,6 +1,33 @@
 /**
  * This package contains the user relevant interfaces and classes for a text
  * based interface of the client.
+ *
+ * <p>
+ * Typically a text based client performs the following steps to establish
+ * a connection:
+ * </p>
+ *
+ * <p>
+ * <code>
+ * // Create a text server and start it.<br>
+ * TextServer server = new TextServer(4242);<br>
+ * server.start();<br>
+ * <br>
+ * // Add a {@link TextListener} to receive the output<br>
+ * server.addTextListener(this);<br>
+ * <br>
+ * // Create a {@link org.jvoicexml.RemoteClient} to be passed to the<br>
+ * // interpreter<br>
+ * RemoteClient client = server.getRemoteClient();<br>
+ * <br>
+ * // Create a session.<br>
+ * Session session = jvxml.createSession(client);
+ * </code>
+ * </p>
+ *
+ * <p>
+ * The {@link TextServer} also serves as an entry point to send the input.
+ * </p>
  * @since 0.6
  */
 
