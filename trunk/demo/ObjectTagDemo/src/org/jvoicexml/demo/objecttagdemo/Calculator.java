482225177647fe30d3515083465b656c23e3c3f2
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2007-11-12 17:30:18 +0000

    Added new demo for object tag execution.

---
+++ trunk/demo/ObjectTagDemo/src/org/jvoicexml/demo/objecttagdemo/Calculator.java
@@ -0,0 +1,51 @@
+/*
+ * File:    $HeadURL$
+ * Version: $LastChangedRevision$
+ * Date:    $Date$
+ * Author:  $LastChangedBy$
+ *
+ * JVoiceXML - A free VoiceXML implementation.
+ *
+ * Copyright (C) 2007 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+package org.jvoicexml.demo.objecttagdemo;
+
+/**
+ * A simple calculator to demo the object execution.
+ *
+ * @author Dirk Schnelle
+ * @version $Revision$
+ * @since 0.6
+ *
+ * <p>
+ * Copyright &copy; 2007 JVoiceXML group - <a
+ * href="http://jvoicexml.sourceforge.net">http://jvoicexml.sourceforge.net/
+ * </a>
+ * </p>
+ */
+public final class Calculator {
+    /**
+     * Adds the given numbers.
+     * @param a first number to add.
+     * @param b second number to add.
+     * @return <code>a+b</code>
+     */
+    public int add(final Integer a, final Integer b) {
+        return a + b;
+    }
+}
