6d852b4ee8f719f77ac058dedfa89be731205182
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2008-01-29 09:12:56 +0000

    Added unit tests for ImplementationPlatform.borrowSystemOutput.

---
+++ trunk/test/unitTests/org/jvoicexml/test/implementationplatform/DummySystemOutput.java
@@ -0,0 +1,62 @@
+/*
+ * File:    $HeadURL: $
+ * Version: $LastChangedRevision:  $
+ * Date:    $Date: $
+ * Author:  $LastChangedBy: $
+ *
+ * JVoiceXML - A free VoiceXML implementation.
+ *
+ * Copyright (C) 2008 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+package org.jvoicexml.test.implementationplatform;
+
+import org.jvoicexml.DocumentServer;
+import org.jvoicexml.SpeakableText;
+import org.jvoicexml.SystemOutput;
+import org.jvoicexml.event.error.BadFetchError;
+import org.jvoicexml.event.error.NoresourceError;
+
+/**
+ * This class provides a dummy implementation of a {@link SystemOutput} for
+ * testing purposes.
+ *
+ * @author Dirk Schnelle
+ * @version $Revision: $
+ * @since 0.6
+ *
+ * <p>
+ * Copyright &copy; 2008 JVoiceXML group - <a
+ * href="http://jvoicexml.sourceforge.net">http://jvoicexml.sourceforge.net/
+ * </a>
+ * </p>
+ */
+public final class DummySystemOutput implements SystemOutput {
+    /**
+     * {@inheritDoc}
+     */
+    public void cancelOutput() throws NoresourceError {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void queueSpeakable(final SpeakableText speakable,
+            final boolean bargein, final DocumentServer documentServer)
+        throws NoresourceError, BadFetchError {
+    }
+}
