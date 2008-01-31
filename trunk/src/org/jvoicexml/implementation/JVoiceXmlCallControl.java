e2e3839da1c85a6fc92aa06cd6745016274e9228
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2008-01-31 09:08:37 +0000

    Continued refactoring of external resource acquisition.

---
+++ trunk/src/org/jvoicexml/implementation/JVoiceXmlCallControl.java
@@ -0,0 +1,108 @@
+/*
+ * File:    $HeadURL: https://jvoicexml.svn.sourceforge.net/svnroot/jvoicexml/trunk/src/org/jvoicexml/implementation/JVoiceXmlUserInput.java $
+ * Version: $LastChangedRevision: 639 $
+ * Date:    $LastChangedDate: 2008-01-29 10:14:00 +0100 (Di, 29 Jan 2008) $
+ * Author:  $LastChangedBy: schnelle $
+ *
+ * JVoiceXML - A free VoiceXML implementation.
+ *
+ * Copyright (C) 2008 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package org.jvoicexml.implementation;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.jvoicexml.CallControl;
+import org.jvoicexml.SystemOutput;
+import org.jvoicexml.UserInput;
+import org.jvoicexml.event.error.NoresourceError;
+
+/**
+ * Basic wrapper for {@link CallControl}.
+ *
+ * @author Dirk Schnelle
+ * @version $Revision: 636 $
+ * @since 0.6
+ *
+ * <p>
+ * Copyright &copy; 2008 JVoiceXML group - <a
+ * href="http://jvoicexml.sourceforge.net"> http://jvoicexml.sourceforge.net/
+ * </a>
+ * </p>
+ */
+final class JVoiceXmlCallControl implements CallControl {
+    /** The encapsulated telephony object. */
+    private final Telephony telephony;
+
+    /**
+     * Constructs a new object.
+     * @param tel encapsulated telephony object.
+     */
+    public JVoiceXmlCallControl(final Telephony tel) {
+        telephony = tel;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void play(final SystemOutput output,
+            final Map<String, String> parameters)
+            throws NoresourceError, IOException {
+        telephony.play(output, parameters);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void record(final UserInput input,
+            final Map<String, String> parameters)
+            throws NoresourceError, IOException {
+        telephony.record(input, parameters);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void stopPlay() throws NoresourceError {
+        telephony.stopPlay();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void stopRecord() throws NoresourceError {
+        telephony.stopRecord();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void transfer(final String dest) throws NoresourceError {
+        telephony.transfer(dest);
+    }
+
+    /**
+     * Retrieves the encapsulated telephony object.
+     * @return the encapsulated telephony object.
+     */
+    public Telephony getTelephony() {
+        return telephony;
+    }
+}
---
+++ trunk/src/org/jvoicexml/implementation/Telephony.java
@@ -0,0 +1,112 @@
+/*
+ * File:    $HeadURL: https://jvoicexml.svn.sourceforge.net/svnroot/jvoicexml/trunk/src/org/jvoicexml/CallControl.java $
+ * Version: $LastChangedRevision: 641 $
+ * Date:    $Date: 2008-01-29 10:15:10 +0100 (Di, 29 Jan 2008) $
+ * Author:  $LastChangedBy: schnelle $
+ *
+ * JVoiceXML - A free VoiceXML implementation.
+ *
+ * Copyright (C) 2008 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package org.jvoicexml.implementation;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.jvoicexml.SystemOutput;
+import org.jvoicexml.UserInput;
+import org.jvoicexml.event.error.NoresourceError;
+
+
+/**
+ * Telephony support.
+ *
+ * <p>
+ * Objects that implement this interface are able to support making a third
+ * party connection through a communications network, such as the telephone.
+ * </p>
+ *
+ *
+ * @author Dirk Schnelle
+ * @version $Revision: 641 $
+ *
+ * <p>
+ * Copyright &copy; 2008 JVoiceXML group -
+ * <a href="http://jvoicexml.sourceforge.net">
+ * http://jvoicexml.sourceforge.net/</a>
+ * </p>
+ */
+public interface Telephony
+        extends ExternalResource {
+    /**
+     * Plays a stream from the given output device.
+     *
+     * <p>
+     * The play method is expected to run asynchronously.
+     * </p>
+     *
+     * @param output the output device delivering the output.
+     * @param parameters parameters to use for playing.
+     * @exception NoresourceError
+     *            Error accessing the terminal
+     * @exception IOException
+     *            Error accessing the given URI.
+     * @since 0.6
+     */
+    void play(final SystemOutput output, final Map<String, String> parameters)
+        throws NoresourceError, IOException;
+
+    /**
+     * Starts recording to the given URI.
+     * @param input input device to use for recording.
+     * @param parameters parameters to use for the recording.
+     * @exception NoresourceError
+     *            Error accessing the terminal
+     * @exception IOException
+     *            Error accessing the given URI.
+     * @since 0.6
+     */
+    void record(final UserInput input, final Map<String, String> parameters)
+        throws NoresourceError, IOException;
+
+    /**
+     * Stops a previously started recording.
+     * @exception NoresourceError
+     *            Error accessing the terminal
+     * @since 0.6
+     */
+    void stopRecord() throws NoresourceError;
+
+    /**
+     * Stops a previously started play.
+     * @exception NoresourceError
+     *            Error accessing the terminal
+     * @since 0.6
+     */
+    void stopPlay() throws NoresourceError;
+
+
+    /**
+     * Transfers the current call.
+     * @param dest Platform specific destination address
+     * @throws NoresourceError
+     *         Error transferring the call.
+     */
+    void transfer(String dest) throws NoresourceError;
+}
