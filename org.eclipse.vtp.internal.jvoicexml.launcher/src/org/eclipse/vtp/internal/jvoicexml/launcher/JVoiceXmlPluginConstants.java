d58d55b96464bf1db1c7859221fc6da0153a4d64
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2008-04-24 07:58:34 +0000

    Initial import.

---
+++ org.eclipse.vtp.internal.jvoicexml.launcher/src/org/eclipse/vtp/internal/jvoicexml/launcher/JVoiceXmlPluginConstants.java
@@ -0,0 +1,46 @@
+/*
+ * JVoiceXML VTP Plugin
+ *
+ * Copyright (C) 2006 Dirk Schnelle
+ *
+ * Copyright (c) 2006 Dirk Schnelle
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.eclipse.vtp.internal.jvoicexml.launcher;
+
+/**
+ * Constants for the plugin used in the <code>JVoiceXmlBrowser</code>.
+ *
+ * @author Dirk Schnelle
+ *
+ * @see org.eclipse.vtp.internal.jvoicexml.launcher.JVoiceXmlBrowser
+ */
+interface JVoiceXmlPluginConstants {
+    /** Prefix of all constants. */
+    String PREFIX = "org.eclipse.vtp.internal.jvoicexml.launching";
+
+    /** Name of the application. */
+    String APPLICATION_NAME = PREFIX + ".general.name";
+
+    /** Location of the security policy. */
+    String JNDI_POLICY = PREFIX + ".jndi.policy";
+
+    /** URL of the codebase. */
+    String JNDI_CODEBASE = PREFIX + ".jndi.codebase";
+
+    /** JNDI Initial context factory. */
+    String JNDI_CONTEXT_FACTORY = PREFIX + ".jndi.context.factory";
+
+    /** RMI provider URL. */
+    String JNDI_PROVIDER_URL = PREFIX + ".jndi.provider.url";
+
+    /** Port of the logging receiver. */
+    String LOGGING_PORT = PREFIX + ".logging.port";
+
+    /** Level of the logger. */
+    String LOGGING_LEVEL = PREFIX + ".logging.level";
+}
