f85776fd644052318677e3ad1896c19b9263bfd7
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2008-04-29 08:43:12 +0000

    Moved to own project.

--- org.jvoicexml/src/org/jvoicexml/implementation/text/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package contains the user relevant interfaces and classes for a text
- * based implementation platform.
- *
- * @since 0.6
- */
-
-package org.jvoicexml.implementation.text;
