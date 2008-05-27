5981a9c30c8b40a64d9e60bb115cf2e8ed1dbb5c
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2008-05-27 07:42:22 +0000

    Update of jsmooth and izpack.

--- trunk/org.jvoicexml/etc/shutdown.bat
+++ trunk/org.jvoicexml/etc/shutdown.bat
@@ -1,3 +1,4 @@
+@echo off 
 rem Normally, editing this script should not be required.
 rem Only case is to set up JAVA_HOME if it's not allready defined.
 rem
@@ -21,8 +22,9 @@ set JVOICEXML_HOME=%~dp0..
 set JVOICEXML_LIB=%JVOICEXML_HOME%\lib
 
 set LOCAL_CLASSPATH="%JVOICEXML_HOME%\config"
-set LOCAL_CLASSPATH=%LOCAL_CLASSPATH%;"%JVOICEXML_LIB%\log4j-1.2.13.jar"
+set LOCAL_CLASSPATH=%LOCAL_CLASSPATH%;"%JVOICEXML_LIB%\log4j-1.2.15.jar"
 set LOCAL_CLASSPATH=%LOCAL_CLASSPATH%;"%JVOICEXML_LIB%\jvxml.jar"
+set LOCAL_CLASSPATH=%LOCAL_CLASSPATH%;"%JVOICEXML_LIB%\jvxml-client.jar"
 
 cd %JVOICEXML_HOME%
 
