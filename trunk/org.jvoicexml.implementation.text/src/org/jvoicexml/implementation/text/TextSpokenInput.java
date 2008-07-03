5e173e8a0bf4abc4e7da30e7f93352e688baa1c7
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2008-07-03 07:38:34 +0000

    Cleanup in passivate() methods of resources. Made the session accessible from the audio file output resources. AudioFIleOutput is an optional resource.

--- trunk/org.jvoicexml.implementation.text/src/org/jvoicexml/implementation/text/TextSpokenInput.java
+++ trunk/org.jvoicexml.implementation.text/src/org/jvoicexml/implementation/text/TextSpokenInput.java
@@ -6,7 +6,7 @@
  *
  * JVoiceXML - A free VoiceXML implementation.
  *
- * Copyright (C) 2007 JVoiceXML group - http://jvoicexml.sourceforge.net
+ * Copyright (C) 2007-2008 JVoiceXML group - http://jvoicexml.sourceforge.net
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Library General Public
@@ -60,12 +60,6 @@ import org.xml.sax.SAXException;
  * @author Dirk Schnelle
  * @version $Revision$
  * @since 0.6
- *
- * <p>
- * Copyright &copy; 2007 JVoiceXML group - <a
- * href="http://jvoicexml.sourceforge.net"> http://jvoicexml.sourceforge.net/
- * </a>
- * </p>
  */
 final class TextSpokenInput implements SpokenInput, ObservableSpokenInput {
     /** Logger for this class. */
@@ -166,6 +160,8 @@ final class TextSpokenInput implements SpokenInput, ObservableSpokenInput {
      * {@inheritDoc}
      */
     public void passivate() {
+        listener.clear();
+        recognizing = false;
     }
 
     /**
