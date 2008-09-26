d84c2c8bbc2bda14db1e052c9d5155746ea50627
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2008-09-26 07:40:46 +0000

    Base implementation for accept in the RuleGrammar.

--- org.jvoicexml/src/org/jvoicexml/implementation/jsapi10/RuleGrammarImplementation.java
+++ org.jvoicexml/src/org/jvoicexml/implementation/jsapi10/RuleGrammarImplementation.java
@@ -26,8 +26,14 @@
 
 package org.jvoicexml.implementation.jsapi10;
 
+import javax.speech.recognition.Rule;
+import javax.speech.recognition.RuleAlternatives;
 import javax.speech.recognition.RuleGrammar;
+import javax.speech.recognition.RuleName;
+import javax.speech.recognition.RuleSequence;
+import javax.speech.recognition.RuleToken;
 
+import org.apache.log4j.Logger;
 import org.jvoicexml.GrammarImplementation;
 import org.jvoicexml.RecognitionResult;
 import org.jvoicexml.xml.srgs.GrammarType;
@@ -48,6 +54,10 @@ import org.jvoicexml.xml.srgs.GrammarType;
  */
 public final class RuleGrammarImplementation
     implements GrammarImplementation<RuleGrammar> {
+    /** Logger for this class. */
+    private static final Logger LOGGER =
+            Logger.getLogger(RuleGrammarImplementation.class);
+
     /** The encapsulated grammar. */
     private final RuleGrammar grammar;
 
@@ -85,7 +95,120 @@ public final class RuleGrammarImplementation
      * {@inheritDoc}
      */
     public boolean accepts(final RecognitionResult result) {
-        // TODO Auto-generated method stub
-        return true;
+        if (grammar == null) {
+            return false;
+        }
+        final String name = grammar.getName();
+        final Rule root = grammar.getRule(name);
+        if (root == null) {
+            return false;
+        }
+        String[] words = result.getWords();
+        if (words == null) {
+            return false;
+        }
+        final int index = accepts(root, words, 0);
+        return index == words.length;
+    }
+
+    final int accepts(final Rule rule,
+            final String[] words, final int index) {
+        int newIndex = index;
+        if (rule instanceof RuleSequence) {
+            final RuleSequence sequence = (RuleSequence) rule;
+            newIndex = accepts(sequence, words, index);
+        } else if (rule instanceof RuleToken) {
+            final RuleToken token = (RuleToken) rule;
+            newIndex = accepts(token, words, index);
+        } else if (rule instanceof RuleAlternatives) {
+            final RuleAlternatives alternatives = (RuleAlternatives) rule;
+            newIndex = accepts(alternatives, words, index);
+        } else if (rule instanceof RuleName) {
+            final RuleName ref = (RuleName) rule;
+            newIndex = accepts(ref, words, index);
+        }
+        return newIndex;
+    }
+    /**
+     * Checks if the given utterance is matched by the given node.
+     * @param grammar the grammar
+     * @param words the utterance to check.
+     * @param index current word
+     * @param node the current node.
+     * @return the new index if the utterances match, <code>-1</code> otherwise.
+     */
+    private int accepts(final RuleSequence sequence,
+            final String[] words, final int index) {
+        final Rule[] rules = sequence.getRules();
+        int localIndex = index;
+        for (Rule current : rules) {
+            int newIndex = accepts(current, words, localIndex);
+            if (newIndex < 0) {
+                return -1;
+            } else {
+                localIndex = newIndex;
+            }
+        }
+        return localIndex;
+    }
+
+    /**
+     * Checks if the given utterance is matched by the given text.
+     * @param grammar the grammar
+     * @param words the utterance to check.
+     * @param index current word
+     * @param text the current text node
+     * @return the new index if the utterances match, <code>-1</code> otherwise.
+     */
+    private int accepts(final RuleToken token, final String[] words,
+            final int index) {
+        final String value = token.getText().trim();
+        if (value.length() == 0) {
+            return -1;
+        }
+        final String[] content = value.split(" ");
+        if (words.length < content.length) {
+            return -1;
+        }
+        for (int i = 0; i < content.length; i++) {
+            if (!words[index + i].equals(content[i])) {
+                return -1;
+            }
+        }
+        return index + content.length;
+    }
+
+    /**
+     * Checks if the given utterance is matched by the given text.
+     * @param grammar the grammar
+     * @param words the utterance to check.
+     * @param index current word
+     * @param oneOf the current OneOf node
+     * @return the new index if the utterances match, <code>-1</code> otherwise.
+     */
+    private int accepts(final RuleAlternatives alternatives,
+            final String[] words, final int index) {
+        final Rule[] items = alternatives.getRules();
+        for (Rule item : items) {
+            final int newIndex = accepts(item, words, index);
+            if (newIndex >= 0) {
+                return newIndex;
+            }
+        }
+        return -1;
+    }
+    /**
+     * Checks if the given utterance is matched by the given text.
+     * @param grammar the grammar
+     * @param words the utterance to check.
+     * @param index current word
+     * @param ref the current Ruleref node
+     * @return the new index if the utterances match, <code>-1</code> otherwise.
+     */
+    private int accepts(final RuleName ref, final String[] words,
+            final int index) {
+        final String reference = ref.getRuleName();
+        final Rule rule = grammar.getRule(reference);
+        return accepts(rule, words, index);
     }
 }
