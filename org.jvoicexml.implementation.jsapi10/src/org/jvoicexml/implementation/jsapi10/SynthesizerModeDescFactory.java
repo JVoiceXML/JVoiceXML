7a3c94c1052570c38a5c14010affb9a31e514a94
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2008-10-24 07:29:26 +0000

    Moved from main project.

---
+++ org.jvoicexml.implementation.jsapi10/src/org/jvoicexml/implementation/jsapi10/SynthesizerModeDescFactory.java
@@ -0,0 +1,60 @@
+/*
+ * File:    $HeadURL$
+ * Version: $LastChangedRevision$
+ * Date:    $Date$
+ * Author:  $LastChangedBy$
+ *
+ * JVoiceXML - A free VoiceXML implementation.
+ *
+ * Copyright (C) 2007 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package org.jvoicexml.implementation.jsapi10;
+
+import javax.speech.synthesis.SynthesizerModeDesc;
+
+/**
+ * A factory for a {@link SynthesizerModeDesc}.
+ *
+ * <p>
+ * Objects implementing this interface can be used to create a
+ * {@link SynthesizerModeDesc} for the
+ * {@link org.jvoicexml.implementation.SynthesizedOutput} objects that are
+ * created by the
+ * {@link AbstractJsapi10SynthesizedOutputFactory}.
+ * </p>
+ *
+ * @author Dirk Schnelle
+ * @version $Revision$
+ * @since 0.6
+ *
+ * <p>
+ * Copyright &copy; 2007 JVoiceXML group - <a
+ * href="http://jvoicexml.sourceforge.net">http://jvoicexml.sourceforge.net/
+ * </a>
+ * </p>
+ */
+public interface SynthesizerModeDescFactory {
+
+    /**
+     * Creates a new descriptor.
+     * @return configured descriptor.
+     */
+    SynthesizerModeDesc createDescriptor();
+
+}
