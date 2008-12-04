4a7493f890f1e8051141deadd9544394fd78d605
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2008-12-04 08:12:55 +0000

    Started to work on a grammar parser.

---
+++ trunk/org.jvoicexml/src/org/jvoicexml/implementation/grammar/SrgsXmlGrammarParser.java
@@ -0,0 +1,114 @@
+/*
+ * File:    $HeadURL$
+ * Version: $LastChangedRevision$
+ * Date:    $Date$
+ * Author:  $LastChangedBy$
+ *
+ * JVoiceXML - A free VoiceXML implementation.
+ *
+ * Copyright (C) 2008 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package org.jvoicexml.implementation.grammar;
+
+import java.util.Collection;
+
+import org.jvoicexml.event.error.SemanticError;
+import org.jvoicexml.implementation.SrgsXmlGrammarImplementation;
+import org.jvoicexml.xml.Text;
+import org.jvoicexml.xml.XmlNode;
+import org.jvoicexml.xml.srgs.Grammar;
+import org.jvoicexml.xml.srgs.Rule;
+import org.jvoicexml.xml.srgs.SrgsXmlDocument;
+
+/**
+ * Parses an SRGS XML grammar.
+ * @author Dirk Schnelle-Walka
+ * @version $Revision$
+ * @since 0.7
+ */
+public final class SrgsXmlGrammarParser
+    implements GrammarParser<SrgsXmlGrammarImplementation> {
+    /**
+     * Constructs a new object.
+     */
+    public SrgsXmlGrammarParser() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public GrammarGraph parse(final SrgsXmlGrammarImplementation grammar)
+        throws SemanticError {
+        final SrgsXmlDocument document = grammar.getGrammar();
+        if (document == null) {
+            return null;
+        }
+        final Grammar rootGrammar = document.getGrammar();
+        final Rule root = rootGrammar.getRootRule();
+        if (root == null) {
+            return null;
+        }
+        return parse(root);
+    }
+
+    /**
+     * Parses the current node.
+     * @param node the current node to parse.
+     * @return the corresponding graph.
+     */
+    private GrammarGraph parse(final XmlNode node) {
+        final Collection<XmlNode> nodes = node.getChildren();
+        GrammarNode start = null;
+        GrammarNode end = null;
+        for (XmlNode current : nodes) {
+            GrammarNode parsedNode = end;
+            if (current instanceof Text) {
+                final Text text = (Text) current;
+                parsedNode = parse(text);
+            }
+            if (parsedNode != null) {
+                if (end != null) {
+                    end.addArc(parsedNode);
+                }
+                end = parsedNode;
+                if (start == null) {
+                    start = end;
+                }
+            }
+        }
+        if (start == null) {
+            return null;
+        }
+        return new GrammarGraph(start, end);
+    }
+
+    /**
+     * Convenience method to convert a {@link Text} into a {@link GrammarNode}.
+     * @param text the current text node
+     * @return the grammar node, <code>null</code> if the node can be ignored.
+     */
+    private GrammarNode parse(final Text text) {
+        final String value = text.getTextContent().trim();
+        if (value.length() == 0) {
+            // Ignore whitespace.
+            return null;
+        }
+        return new GrammarNode(false, GrammarNode.TOKEN, value);
+    }
+}
---
+++ trunk/org.jvoicexml/src/org/jvoicexml/implementation/grammar/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Classes that facilitate parsing of grammars..
+  */
+
+package org.jvoicexml.implementation.grammar;
