8cad3a36e353d8410483139287bf133959337f9d
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2009-01-27 17:27:46 +0000

    Moving towards a new way of configuration without a dynamic classpath setting.

---
+++ org.jvoicexml/test/unitTests/org/jvoicexml/config/TestJVoiceXmlConfiguration.java
@@ -0,0 +1,56 @@
+/*
+ * File:    $HeadURL$
+ * Version: $LastChangedRevision$
+ * Date:    $Date$
+ * Author:  $LastChangedBy$
+ *
+ * JVoiceXML - A free VoiceXML implementation.
+ *
+ * Copyright (C) 2009 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package org.jvoicexml.config;
+
+import java.io.File;
+import java.util.Collection;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+/**
+ * Test cases for {@link JVoiceXmlConfiguration}.
+ * @author Dirk Schnelle-Walka
+ * @version $Revision$
+ * @since 0.7
+ */
+public final class TestJVoiceXmlConfiguration {
+
+    /**
+     * Test method for {@link org.jvoicexml.config.JVoiceXmlConfiguration#getConfigurationFiles(java.lang.String)}.
+     * @exception Exception
+     *            test failed
+     */
+    @Test
+    public void testGetConfigurationFiles() throws Exception {
+        JVoiceXmlConfiguration config = JVoiceXmlConfiguration.getInstance();
+        Collection<File> files = config.getConfigurationFiles("implementation");
+        Assert.assertEquals(1, files.size());
+    }
+
+}
