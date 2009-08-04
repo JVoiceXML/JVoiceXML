378a355963bc32023292823fd0a141c72b8ff528
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2009-08-04 09:06:00 +0000

    Tagging release 0.7.1.GA

---
+++ core/tags/0.7.1.GA/org.jvoicexml.implementation.jsapi10/unittests/org/jvoicexml/test/implementation/DummyRuleGrammar.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.jvoicexml.test.implementation;
+
+import com.sun.speech.engine.recognition.BaseRuleGrammar;
+
+/**
+ * Basic rule grammar for testing.
+ * @author Dirk Schnelle-Walka
+ */
+@SuppressWarnings("serial")
+public final class DummyRuleGrammar extends BaseRuleGrammar {
+    /**
+     * Constructs a new object.
+     */
+    public DummyRuleGrammar() {
+        super(null, "dummy");
+    }
+
+}
