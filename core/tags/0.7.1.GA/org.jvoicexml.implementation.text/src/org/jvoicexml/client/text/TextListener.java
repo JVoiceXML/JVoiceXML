5debb58fd9928fe0eec8aaf260faeb5dc5ee5479
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2009-08-04 09:07:48 +0000

    Tagging release 0.7.1.GA

---
+++ core/tags/0.7.1.GA/org.jvoicexml.implementation.text/src/org/jvoicexml/client/text/TextListener.java
@@ -0,0 +1,71 @@
+/*
+ * File:    $HeadURL$
+ * Version: $LastChangedRevision$
+ * Date:    $Date$
+ * Author:  $LastChangedBy$
+ *
+ * JVoiceXML - A free VoiceXML implementation.
+ *
+ * Copyright (C) 2007 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package org.jvoicexml.client.text;
+
+import java.net.InetSocketAddress;
+
+import org.jvoicexml.xml.ssml.SsmlDocument;
+
+/**
+ * Listener for text output.
+ *
+ * @author Dirk Schnelle-Walka
+ * @version $Revision$
+ * @since 0.6
+ */
+public interface TextListener {
+    /**
+     * Notification that the server thread has started.
+     * @since 0.7
+     */
+    void started();
+
+    /**
+     * Notification that a connection with the server has been established.
+     * @param remote address of the server.
+     * @since 0.7
+     */
+    void connected(InetSocketAddress remote);
+
+    /**
+     * Notification that a portion of text has arrived.
+     * @param text the received text.
+     */
+    void outputText(final String text);
+
+    /**
+     * Notification that an SSML document has arrived.
+     * @param document the received SSML document.
+     */
+    void outputSsml(final SsmlDocument document);
+
+    /**
+     * Notification about a disconnect from the server.
+     * @since 0.7
+     */
+    void disconnected();
+}
---
+++ core/tags/0.7.1.GA/org.jvoicexml.implementation.text/src/org/jvoicexml/client/text/package-info.java
@@ -0,0 +1,50 @@
+/**
+ * User relevant interfaces and classes for a text based interface of the
+ * client.
+ *
+ * <p>
+ * Typically a text based client performs the following steps to establish
+ * a connection:
+ * </p>
+ *
+ * <p>
+ * <code>
+ * // Create a {@link org.jvoicexml.client.text.TextServer} and start it.<br>
+ * TextServer server = new TextServer(4242);<br>
+ * server.start();<br>
+ * <br>
+ * // Add a {@link org.jvoicexml.client.text.TextListener} to receive the
+ * output<br>
+ * server.addTextListener(this);<br>
+ * <br>
+ * // Create a {@link org.jvoicexml.RemoteClient} to be passed to the<br>
+ * // interpreter<br>
+ * RemoteClient client = server.getRemoteClient();<br>
+ * <br>
+ * // Create a {@link org.jvoicexml.Session}.<br>
+ * Session session = jvxml.createSession(client);
+ * </code>
+ * </p>
+ *
+ * <p>
+ * Note that the user input may not be active if a field's prompt arrives.
+ * </p>
+ *
+ * <p>
+ * The {@link org.jvoicexml.client.text.TextServer} also serves as an entry
+ * point to send the input.
+ * </p>
+ *
+ * <p>
+ * Any user input can be sent to the server via the
+ * {@link org.jvoicexml.client.text.TextServer#sendInput(String)} method.
+ * </p>
+ * <p>
+ * <code>
+ * server.sendInput("this is my input");
+ * </code>
+ * </p>
+ * @since 0.6
+ */
+
+package org.jvoicexml.client.text;
