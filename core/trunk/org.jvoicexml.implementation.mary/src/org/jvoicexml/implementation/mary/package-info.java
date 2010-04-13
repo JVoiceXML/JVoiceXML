31b8cb58e44e955a947957faa404f73417b1954f
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2010-04-13 07:21:08 +0000

    Enhanced javadoc.

--- core/trunk/org.jvoicexml.implementation.mary/src/org/jvoicexml/implementation/mary/package-info.java
+++ core/trunk/org.jvoicexml.implementation.mary/src/org/jvoicexml/implementation/mary/package-info.java
@@ -1,5 +1,15 @@
-
- /** This package contains the classes that enable the.
-   the JVoiceXml interpreter to target the Mary TTS
+/**
+ * This package contains the classes that enable
+ * the JVoiceXml interpreter to target the Mary TTS.
+ *
+ * <p>
+ * Mary is an open source TTS system that is a continuation of FreeTTS. Mary can
+ * be downloaded for free from <a href="mary.dfki.de">http://mary.dfki.de</a>.
+ * </p>
+ * <p>
+ * The Mary TTS server must be running when this implementation platform is
+ * used.
+ * </p>
  */
+
 package org.jvoicexml.implementation.mary;
