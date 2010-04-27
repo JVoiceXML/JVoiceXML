b255137b3a7ccb3717a6135f581b2c62ab7b6728
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2010-04-27 16:39:58 +0000

    Initial import.

---
+++ core/trunk/org.jvoicexml.processor.srgs/src/org/jvoicexml/processor/srgs/GrammarChecker.java
@@ -0,0 +1,172 @@
+/*
+ * File:    $HeadURL: https://jvoicexml.svn.sourceforge.net/svnroot/jvoicexml/core/trunk/org.jvoicexml/src/org/jvoicexml/implementation/grammar/GrammarChecker.java $
+ * Version: $LastChangedRevision: 2129 $
+ * Date:    $Date: 2010-04-09 11:33:10 +0200 (Fr, 09 Apr 2010) $
+ * Author:  $LastChangedBy: schnelle $
+ *
+ * JVoiceXML - A free VoiceXML implementation.
+ *
+ * Copyright (C) 2008 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package org.jvoicexml.processor.srgs;
+
+import java.util.Collection;
+import java.util.Stack;
+
+import org.mozilla.javascript.ScriptableObject;
+
+/**
+ * This class provides a means to perform evaluations on a parsed grammar.
+ * @author Dirk Schnelle-Walka
+ * @version $Revision: 2129 $
+ * @since 0.7
+ */
+public final class GrammarChecker {
+    /** The graph to analyze. */
+    private final GrammarGraph graph;
+
+    /** Matched tokens. */
+    private final Stack<GrammarNode> matchedTokens;
+
+    /**
+     * Constructs a new object.
+     * @param grammarGraph the graph to analyze.
+     */
+    public GrammarChecker(final GrammarGraph grammarGraph) {
+        matchedTokens = new Stack<GrammarNode>();
+        graph = grammarGraph;
+    }
+    /**
+     * Checks if the given tokens can be represented using the given
+     * graph.
+     * @param tokens the tokens
+     * @return <code>true</code> if the tokens are valid.
+     */
+    public boolean isValid(final String[] tokens) {
+        matchedTokens.clear();
+        final GrammarNode start = graph.getStartNode();
+        return isValid(start, tokens)
+            && (matchedTokens.size() == tokens.length);
+    }
+
+    /**
+     * Retrieves the result of the grammar check process. This may differ from
+     * the parsed tokens of the original utterance.
+     * <p>
+     * A call to this method is only valid after a call to
+     * {@link #isValid(String[])} until the next validation check.
+     * </p>
+     * @return interpreteration result
+     * TODO This is just a first attempt to go into the direction of semantic
+     * interpretation and may change.
+     */
+    public ScriptableObject getInterpretation() {
+        Collection<String> result = new java.util.ArrayList<String>();
+        for (GrammarNode node : matchedTokens) {
+            final GrammarNodeType type = node.getType();
+            if (type == GrammarNodeType.TOKEN) {
+                final TokenGrammarNode tokenNode = (TokenGrammarNode) node;
+                final String token = tokenNode.getToken();
+                result.add(token);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Checks if the given node is on the path.
+     * @param node the node
+     * @param tokens the tokens to analyze.
+     * @return <code>true</code> if the node is on the path.
+     */
+    private boolean isValid(final GrammarNode node, final String[] tokens) {
+        final GrammarNodeType type = node.getType();
+        if ((type == GrammarNodeType.GRAPH) || (type == GrammarNodeType.RULE)) {
+            final GrammarGraph currentGraph = (GrammarGraph) node;
+            if (currentGraph.getMinRepeat() == 0) {
+                final GrammarNode end = currentGraph.getEndNode();
+                if (isValid(end, tokens)) {
+                    return true;
+                }
+            }
+            final GrammarNode start = currentGraph.getStartNode();
+            return isValid(start, tokens);
+        }
+
+        boolean pushedNode = false;
+        if (type == GrammarNodeType.TOKEN) {
+            if (matchedTokens.size() >= tokens.length) {
+                return false;
+            }
+            final TokenGrammarNode tokenNode = (TokenGrammarNode) node;
+            final String token = tokenNode.getToken();
+            if (token.equals(tokens[matchedTokens.size()])) {
+                matchedTokens.push(node);
+                pushedNode = true;
+                if (matchedTokens.size() >= tokens.length) {
+                    return isFinalNode(node);
+                }
+            } else {
+                if (tokenNode.getMinRepeat() > 0) {
+                    return false;
+                }
+            }
+        }
+
+        for (GrammarNode destination : node.getNextNodes()) {
+            if (isValid(destination, tokens)) {
+                return true;
+            }
+        }
+        if (pushedNode && (matchedTokens.size() > 0)) {
+            matchedTokens.pop();
+        }
+        return false;
+    }
+
+    /**
+     * Checks if the given node is a final node. This is the case if either the
+     * given node is a final node itself or if there is a way through pure
+     * {@link EmptyGrammarNode}s to a final node.
+     * @param node the node to inspect.
+     * @return <code>true</code> if the given node is a final node.
+     */
+    private boolean isFinalNode(final GrammarNode node) {
+        if (node.isFinalNode()) {
+            return true;
+        }
+        for (GrammarNode destination : node.getNextNodes()) {
+            final GrammarNodeType type = destination.getType();
+            if (type == GrammarNodeType.GRAPH) {
+                final GrammarGraph currentGraph = (GrammarGraph) destination;
+                final GrammarNode start = currentGraph.getStartNode();
+                if (isFinalNode(start)) {
+                    return true;
+                }
+            } else if (type == GrammarNodeType.TOKEN) {
+                return false;
+            } else {
+                if (isFinalNode(destination)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ core/trunk/org.jvoicexml.processor.srgs/unittests/org/jvoicexml/processor/srgs/TestGrammarChecker.java
@@ -0,0 +1,112 @@
+/*
+ * File:    $HeadURL: https://jvoicexml.svn.sourceforge.net/svnroot/jvoicexml/core/trunk/org.jvoicexml/test/unitTests/org/jvoicexml/implementation/grammar/TestGrammarChecker.java $
+ * Version: $LastChangedRevision: 2153 $
+ * Date:    $Date: 2010-04-14 09:25:59 +0200 (Mi, 14 Apr 2010) $
+ * Author:  $LastChangedBy: schnelle $
+ *
+ * JVoiceXML - A free VoiceXML implementation.
+ *
+ * Copyright (C) 2009 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package org.jvoicexml.processor.srgs;
+
+import java.util.Collection;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.jvoicexml.xml.srgs.Grammar;
+import org.jvoicexml.xml.srgs.Item;
+import org.jvoicexml.xml.srgs.OneOf;
+import org.jvoicexml.xml.srgs.Rule;
+import org.jvoicexml.xml.srgs.SrgsXmlDocument;
+import org.jvoicexml.xml.srgs.Tag;
+
+/**
+ * Test cases for {@link GrammarChecker}.
+ * @author Dirk Schnelle-Walka
+ * @version $Revision: 2153 $
+ * @since 0.7
+ */
+public final class TestGrammarChecker {
+
+    /**
+     * Test method for {@link org.jvoicexml.implementation.grammar.GrammarChecker#getInterpretation()}.
+     * @exception Exception test failed
+     */
+    @Test
+    public void testGetInterpretation() throws Exception {
+        final SrgsXmlDocument document = new SrgsXmlDocument();
+        final Grammar grammar = document.getGrammar();
+        grammar.setRoot("drink");
+        final Rule rule = grammar.appendChild(Rule.class);
+        rule.setId("drink");
+        final OneOf oneof = rule.appendChild(OneOf.class);
+        final Item cokeItem = oneof.appendChild(Item.class);
+        cokeItem.addText("coke");
+        final Item pepsiItem = oneof.appendChild(Item.class);
+        pepsiItem.addText("pepsi");
+        final Item cocaColaItem = oneof.appendChild(Item.class);
+        cocaColaItem.addText("coca cola");
+        final Tag tag = cocaColaItem.appendChild(Tag.class);
+        tag.addText("coke");
+        System.out.println(document);
+
+        final SrgsXmlGrammarParser parser = new SrgsXmlGrammarParser();
+        final GrammarGraph graph = parser.parse(document);
+        dump(graph, 2);
+        final GrammarChecker checker = new GrammarChecker(graph);
+        String[] words = new String[] {"coke"};
+        Assert.assertTrue(checker.isValid(words));
+        System.out.println(checker.getInterpretation());
+    }
+
+    private void dump(final GrammarNode node, int indent) {
+        if (indent > 30) {
+            return;
+        }
+        for (int i = 0; i<indent; i++) {
+            System.out.print(' ');
+        }
+        System.out.print(node.getType() + "\tmin: " + node.getMinRepeat()
+                + "\tmax: " + node.getMaxRepeat());
+        if (node instanceof RuleNode) {
+            RuleNode ruleNode = (RuleNode) node;
+            System.out.print("\tid: " + ruleNode.getId());
+        }
+        if (node instanceof TokenGrammarNode) {
+            TokenGrammarNode token = (TokenGrammarNode) node;
+            System.out.print("\t'" + token.getToken() + "'");
+        }
+        if (node instanceof TagGrammarNode) {
+            TagGrammarNode tag = (TagGrammarNode) node;
+            System.out.print("\t'" + tag.getTag() + "'");
+        }
+        System.out.println("");
+        if (node instanceof GrammarGraph) {
+            GrammarGraph graph = (GrammarGraph) node;
+            dump(graph.getStartNode(), indent);
+        } else {
+            Collection<GrammarNode> arcs = node.getNextNodes();
+            for (GrammarNode current : arcs) {
+                dump(current, indent + 2);
+            }
+        }
+    }
+}
