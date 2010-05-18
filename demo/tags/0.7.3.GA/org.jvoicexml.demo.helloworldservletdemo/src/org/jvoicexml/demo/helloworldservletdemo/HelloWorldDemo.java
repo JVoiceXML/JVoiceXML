62ff918d1baae6bbd13a3b1ea0cfb8354ce491ac
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2010-05-18 09:20:23 +0000

    Tagging release 0.7.3.GA

---
+++ demo/tags/0.7.3.GA/org.jvoicexml.demo.helloworldservletdemo/src/org/jvoicexml/demo/helloworldservletdemo/HelloWorldDemo.java
@@ -0,0 +1,140 @@
+/*
+ * File:    $HeadURL$
+ * Version: $LastChangedRevision$
+ * Date:    $Date$
+ * Author:  $LastChangedBy$
+ *
+ * JVoiceXML Demo - Demo for the free VoiceXML implementation JVoiceXML
+ *
+ * Copyright (C) 2005-2009 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package org.jvoicexml.demo.helloworldservletdemo;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+import org.apache.log4j.Logger;
+import org.jvoicexml.JVoiceXml;
+import org.jvoicexml.RemoteClient;
+import org.jvoicexml.Session;
+import org.jvoicexml.client.BasicRemoteClient;
+import org.jvoicexml.event.JVoiceXMLEvent;
+
+/**
+ * Demo implementation of the venerable "Hello World". This demo requires a
+ * servlet container to deliver the VoiceXML documents.
+ *
+ * @author Dirk Schnelle-Walka
+ * @version $Revision$
+ */
+public final class HelloWorldDemo {
+    /** Logger for this class. */
+    private static final Logger LOGGER = Logger.getLogger(HelloWorldDemo.class);
+
+    /** The JNDI context. */
+    private Context context;
+
+    /**
+     * Do not create from outside.
+     */
+    private HelloWorldDemo() {
+        try {
+            context = new InitialContext();
+        } catch (javax.naming.NamingException ne) {
+            LOGGER.error("error creating initial context", ne);
+
+            context = null;
+        }
+    }
+
+    /**
+     * Calls the VoiceXML interpreter context to process the given xml document.
+     * @param uri URI of the first document to load
+     * @exception JVoiceXMLEvent
+     *            Error processing the call.
+     */
+    private void interpretDocument(final URI uri)
+        throws JVoiceXMLEvent {
+        JVoiceXml jvxml;
+        try {
+            jvxml = (JVoiceXml) context.lookup("JVoiceXml");
+        } catch (javax.naming.NamingException ne) {
+            LOGGER.error("error obtaining JVoiceXml", ne);
+
+            return;
+        }
+
+        final RemoteClient client = new BasicRemoteClient("jsapi10", "jsapi10",
+            "jsapi10");
+        final Session session = jvxml.createSession(client);
+
+        session.call(uri);
+
+        session.waitSessionEnd();
+
+        session.hangup();
+    }
+
+    /**
+     * The main method.
+     *
+     * @param args
+     * Command line arguments.
+     * <ol>
+     * <li><code>&lt;URL&gt;</code> URL of the application root document.
+     * </ol>
+     */
+    public static void main(final String[] args) {
+        LOGGER.info("Starting 'hello world' servlet demo for JVoiceXML...");
+        LOGGER.info("(c) 2005-2009 by JVoiceXML group - "
+                + "http://jvoicexml.sourceforge.net/");
+
+        if (args.length != 1) {
+            System.out.println("usage:");
+            System.out.println("java " + HelloWorldDemo.class.getName()
+                    + " <URL>");
+            System.exit(-1);
+        }
+
+        final HelloWorldDemo demo = new HelloWorldDemo();
+
+        URI uri = null;
+        try {
+            final URL url = new URL(args[0]);
+            uri = url.toURI();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+        try {
+            demo.interpretDocument(uri);
+        } catch (org.jvoicexml.event.JVoiceXMLEvent e) {
+            LOGGER.error("error processing the document", e);
+        }
+    }
+}
