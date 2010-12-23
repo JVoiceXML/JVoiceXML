c61f72bb241b4a5d64422033f9f91d5e1d92e437
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2010-12-23 12:45:26 +0000

    Added error code to error message.

--- core/trunk/org.jvoicexml/src/org/jvoicexml/documentserver/schemestrategy/HttpSchemeStrategy.java
+++ core/trunk/org.jvoicexml/src/org/jvoicexml/documentserver/schemestrategy/HttpSchemeStrategy.java
@@ -162,7 +162,8 @@ public final class HttpSchemeStrategy
             final StatusLine statusLine = response.getStatusLine();
             final int status = statusLine.getStatusCode();
             if (status != HttpStatus.SC_OK) {
-                throw new BadFetchError(statusLine.getReasonPhrase());
+                throw new BadFetchError("Error: " + status + " "
+                        + statusLine.getReasonPhrase());
             }
             final HttpEntity entity = response.getEntity();
             return entity.getContent();
