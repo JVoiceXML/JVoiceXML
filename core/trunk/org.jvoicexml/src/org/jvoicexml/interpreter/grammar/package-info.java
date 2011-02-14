79c1c4d36559a581321abdec8a249e481326f79c
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2011-02-14 14:13:08 +0000

    Updated package description.

--- core/trunk/org.jvoicexml/src/org/jvoicexml/interpreter/grammar/package-info.java
+++ core/trunk/org.jvoicexml/src/org/jvoicexml/interpreter/grammar/package-info.java
@@ -1,5 +1,7 @@
 /**
  * This package contains the classes to handle the grammars.
+ * grammars can be either internal and external and must be converted
+ * to a unique format so that they can be handled by the recognizers.
  */
 
 package org.jvoicexml.interpreter.grammar;
