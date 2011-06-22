04a1c12909e0e0853756e65f62cda34a0335956c
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2011-06-22 15:41:35 +0000

    Started to work on a marc implementation platform.

---
+++ core/trunk/org.jvoicexml.implementation.marc/src/org/jvoicexml/implementation/marc/MarcSynthesizedOutput.java
@@ -0,0 +1,212 @@
+/*
+ * File:    $HeadURL:  $
+ * Version: $LastChangedRevision: 643 $
+ * Date:    $Date: $
+ * Author:  $LastChangedBy: $
+ *
+ * JVoiceXML - A free VoiceXML implementation.
+ *
+ * Copyright (C) 2010 JVoiceXML group - http://jvoicexml.sourceforge.net
+ * The JVoiceXML group hereby disclaims all copyright interest in the
+ * library `JVoiceXML' (a free VoiceXML implementation).
+ * JVoiceXML group, $Date: $, Dirk Schnelle-Walka, project lead
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package org.jvoicexml.implementation.marc;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.jvoicexml.ConnectionInformation;
+import org.jvoicexml.DocumentServer;
+import org.jvoicexml.SpeakableText;
+import org.jvoicexml.event.error.BadFetchError;
+import org.jvoicexml.event.error.NoresourceError;
+import org.jvoicexml.implementation.SynthesizedOutput;
+import org.jvoicexml.implementation.SynthesizedOutputListener;
+
+/**
+ * Demo implementation for a synthesized output for MARC.
+ * 
+ * @author Dirk Schnelle-Walka
+ * @since 0.7.4
+ */
+public class MarcSynthesizedOutput implements SynthesizedOutput {
+    /** Type of the created resources. */
+    private String type;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Sets the type of the created resource.
+     * @param typeName
+     */
+    void setType(final String typeName) {
+        type = typeName;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void open() throws NoresourceError {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void activate() throws NoresourceError {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void passivate() throws NoresourceError {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void close() {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isBusy() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void connect(ConnectionInformation client) throws IOException {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void disconnect(ConnectionInformation client) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean supportsBargeIn() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void cancelOutput() throws NoresourceError {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addListener(SynthesizedOutputListener listener) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void removeListener(SynthesizedOutputListener listener) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.jvoicexml.implementation.SynthesizedOutput#
+     * getUriForNextSynthesisizedOutput()
+     */
+    @Override
+    public URI getUriForNextSynthesisizedOutput() throws NoresourceError,
+            URISyntaxException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void queueSpeakable(final SpeakableText speakable,
+            final String sessionId, final DocumentServer documentServer)
+        throws NoresourceError,
+            BadFetchError {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void waitNonBargeInPlayed() {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void waitQueueEmpty() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ core/trunk/org.jvoicexml.implementation.marc/src/org/jvoicexml/implementation/marc/MarcSynthesizedOutputFactory.java
@@ -0,0 +1,108 @@
+/*
+ * File:    $HeadURL: https://jvoicexml.svn.sourceforge.net/svnroot/jvoicexml/core/trunk/org.jvoicexml.implementation.jsapi20/src/org/jvoicexml/implementation/jsapi20/Jsapi20SynthesizedOutputFactory.java $
+ * Version: $LastChangedRevision: 2355 $
+ * Date:    $Date: 2010-10-07 20:28:03 +0200 (Do, 07 Okt 2010) $
+ * Author:  $LastChangedBy: schnelle $
+ *
+ * JVoiceXML - A free VoiceXML implementation.
+ *
+ * Copyright (C) 2006-2010 JVoiceXML group - http://jvoicexml.sourceforge.net
+ * The JVoiceXML group hereby disclaims all copyright interest in the
+ * library `JVoiceXML' (a free VoiceXML implementation).
+ * JVoiceXML group, $Date: 2010-10-07 20:28:03 +0200 (Do, 07 Okt 2010) $, Dirk Schnelle-Walka, project lead
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package org.jvoicexml.implementation.marc;
+
+import org.jvoicexml.event.error.NoresourceError;
+import org.jvoicexml.implementation.ResourceFactory;
+import org.jvoicexml.implementation.SynthesizedOutput;
+
+/**
+ * Demo implementation of a {@link org.jvoicexml.implementation.ResourceFactory}
+ * for the {@link org.jvoicexml.implementation.SynthesizedOutput} for MARC.
+ *
+ * @author Dirk Schnelle-Walka
+ * @version $Revision: 2355 $
+ * @since 0.5.5
+ */
+public final class MarcSynthesizedOutputFactory
+        implements ResourceFactory<SynthesizedOutput> {
+    /** Number of instances that this factory will create. */
+    private int instances;
+
+    /** Type of the created resources. */
+    private String type;
+
+    /**
+     * Constructs a new object.
+     * @param engineFactory class name of the engine list factory.
+     */
+    public MarcSynthesizedOutputFactory() {
+        type = "marc";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public SynthesizedOutput createResource() throws NoresourceError {
+        final MarcSynthesizedOutput output = new MarcSynthesizedOutput();
+        output.setType(type);
+        return output;
+    }
+
+    /**
+     * Sets the number of instances that this factory will create.
+     *
+     * @param number
+     *                Number of instances to create.
+     */
+    public void setInstances(final int number) {
+        instances = number;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int getInstances() {
+        return instances;
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Sets the type of the created resource.
+     * @param typeName
+     */
+    void setType(final String typeName) {
+        type = typeName;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Class<SynthesizedOutput> getResourceType() {
+        return SynthesizedOutput.class;
+    }
+}
