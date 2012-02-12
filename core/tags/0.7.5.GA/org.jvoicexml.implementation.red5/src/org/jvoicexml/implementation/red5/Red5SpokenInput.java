1bc64c9df95c0f4b264535c5acd8e36f948577a6
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2012-02-12 14:43:28 +0000

    Tagging release 0.7.5.GA

---
+++ core/tags/0.7.5.GA/org.jvoicexml.implementation.red5/src/org/jvoicexml/implementation/red5/Red5SpokenInput.java
@@ -0,0 +1,238 @@
+/*
+ * File:    $HeadURL:  $
+ * Version: $LastChangedRevision: 643 $
+ * Date:    $Date: $
+ * Author:  $LastChangedBy: $
+ *
+ * JVoiceXML - A free VoiceXML implementation.
+ *
+ * Copyright (C) 2010 JVoiceXML group - http://jvoicexml.sourceforge.net
+ * The JVoiceXML group hereby disclaims all copyright interest in the
+ * library `JVoiceXML' (a free VoiceXML implementation).
+ * JVoiceXML group, $Date: $, Dirk Schnelle-Walka, project lead
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package org.jvoicexml.implementation.red5;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collection;
+
+import org.jvoicexml.ConnectionInformation;
+import org.jvoicexml.DtmfRecognizerProperties;
+import org.jvoicexml.SpeechRecognizerProperties;
+import org.jvoicexml.event.error.BadFetchError;
+import org.jvoicexml.event.error.NoresourceError;
+import org.jvoicexml.event.error.UnsupportedFormatError;
+import org.jvoicexml.event.error.UnsupportedLanguageError;
+import org.jvoicexml.implementation.GrammarImplementation;
+import org.jvoicexml.implementation.SpokenInput;
+import org.jvoicexml.implementation.SpokenInputListener;
+import org.jvoicexml.xml.srgs.GrammarType;
+import org.jvoicexml.xml.vxml.BargeInType;
+
+/**
+ * Demo implementation for a synthesized output using the Red5 server.
+ *
+ * @author Dirk Schnelle-Walka
+ * @author Mohamed Ali
+ * @since 0.7.4
+ */
+public final class Red5SpokenInput implements SpokenInput {
+    /** Type of the created resources. */
+    private String type;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Sets the type of the created resource.
+     * @param typeName
+     */
+    void setType(final String typeName) {
+        type = typeName;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void open() throws NoresourceError {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void activate() throws NoresourceError {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void passivate() throws NoresourceError {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void close() {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isBusy() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void connect(ConnectionInformation client) throws IOException {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void disconnect(ConnectionInformation client) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void startRecognition(
+            final SpeechRecognizerProperties speech,
+            final DtmfRecognizerProperties dtmf)
+        throws NoresourceError, BadFetchError {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void stopRecognition() {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addListener(SpokenInputListener listener) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void removeListener(SpokenInputListener listener) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void activateGrammars(Collection<GrammarImplementation<?>> grammars)
+            throws BadFetchError, UnsupportedLanguageError, NoresourceError {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void deactivateGrammars(Collection<GrammarImplementation<?>> grammars)
+            throws NoresourceError, BadFetchError {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public URI getUriForNextSpokenInput() throws NoresourceError,
+            URISyntaxException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Collection<GrammarType> getSupportedGrammarTypes() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public GrammarImplementation<?> loadGrammar(Reader reader, GrammarType type)
+            throws NoresourceError, BadFetchError, UnsupportedFormatError {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Collection<BargeInType> getSupportedBargeInTypes() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
