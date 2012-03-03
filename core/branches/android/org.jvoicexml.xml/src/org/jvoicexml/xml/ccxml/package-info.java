666c6e68c55beef22d546c0125b0214bb51514ea
marakatu <marakatu@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2012-03-03 20:30:05 +0000

    updating the core

--- core/branches/android/org.jvoicexml.xml/src/org/jvoicexml/xml/ccxml/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package contains the classes to handle CCXML documents and
- * related tags. CCXML is specified at
- * <a href="http://www.w3.org/TR/ccxml">http://www.w3.org/TR/ccxml</a>.
- */
-
-package org.jvoicexml.xml.ccxml;
-
