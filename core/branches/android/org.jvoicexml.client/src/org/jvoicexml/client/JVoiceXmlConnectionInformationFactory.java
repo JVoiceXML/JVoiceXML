fd0855d5704aaf29bab62fcd684b7e0589b59d17
marakatu <marakatu@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2012-11-09 05:27:33 +0000

    uploading updated content

---
+++ core/branches/android/org.jvoicexml.client/src/org/jvoicexml/client/JVoiceXmlConnectionInformationFactory.java
@@ -0,0 +1,105 @@
+/*
+ * File:    $HeadURL:  $
+ * Version: $LastChangedRevision: 643 $
+ * Date:    $Date: $
+ * Author:  $LastChangedBy: $
+ *
+ * JVoiceXML - A free VoiceXML implementation.
+ *
+ * Copyright (C) 2012 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+package org.jvoicexml.client;
+
+import org.jvoicexml.CallControl;
+import org.jvoicexml.UserInput;
+
+/**
+ * {@link ConnectionInformationFactory} for the default identifiers
+ * deployed with JVoiceXML.
+ * @author Dirk Schnelle-Walka
+ * @version $Revision: $
+ * @since 0.7.6
+ */
+public final class JVoiceXmlConnectionInformationFactory
+        implements ConnectionInformationFactory {
+    /**
+     * Checks if the the given identifier is among the given list of
+     * identifiers.
+     * @param identifier the identifier to check
+     * @param identifiers the list of available identifiers
+     * @param resource the resource to check
+     * @throws UnsupportedResourceIdentifierException
+     *         if the identifier is not among the list of given identifiers
+     */
+    private void checkIdentifiers(final String identifier,
+            final String[] identifiers, Class<?> resource)
+                    throws UnsupportedResourceIdentifierException {
+        for (String current : identifiers) {
+            if (current.equals(identifier)) {
+                return;
+            }
+        }
+        final String message = String.format("%s is not supported for type %s",
+                identifier, resource.getCanonicalName());
+        throw new UnsupportedResourceIdentifierException(message);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ConnectionInformationController createConnectionInformation(
+            final String call, final String output, final String input)
+                    throws UnsupportedResourceIdentifierException {
+        // First check if all requested resources are available.
+        final String[] calls = getCallControlIdentifiers();
+        checkIdentifiers(call, calls, CallControl.class);
+        final String[] outputs = getSystemOutputIdentifiers();
+        checkIdentifiers(output, outputs, CallControl.class);
+        final String[] inputs = getUserInputIdentifiers();
+        checkIdentifiers(input, inputs, UserInput.class);
+
+        // If successful: create a new connection info
+        return new BasicConnectionInformationController(call, output, input);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String[] getCallControlIdentifiers() {
+        return new String[] { "dummy" };
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String[] getSystemOutputIdentifiers() {
+        return new String[] { "jsapi10", "jsapi20", "mary", "marc" };
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String[] getUserInputIdentifiers() {
+        return new String[] { "jsapi10", "jsapi20", "mary", "marc" };
+    }
+
+}
---
+++ core/branches/android/org.jvoicexml.client/src/org/jvoicexml/client/rtp/RtpPlayer.java
@@ -0,0 +1,144 @@
+/*
+ * File:    $HeadURL: https://jvoicexml.svn.sourceforge.net/svnroot/jvoicexml/core/trunk/org.jvoicexml/src/org/jvoicexml/client/rtp/RtpPlayer.java $
+ * Version: $LastChangedRevision: 2129 $
+ * Date:    $Date: 2010-04-09 11:33:10 +0200 (Fr, 09 Apr 2010) $
+ * Author:  $LastChangedBy: schnelle $
+ *
+ * JVoiceXML - A free VoiceXML implementation.
+ *
+ * Copyright (C) 2007-2008 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package org.jvoicexml.client.rtp;
+
+import java.io.IOException;
+import java.net.DatagramSocket;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+
+import org.jlibrtp.DataFrame;
+import org.jlibrtp.Participant;
+import org.jlibrtp.RTPAppIntf;
+import org.jlibrtp.RTPSession;
+
+/**
+ * RTP player for playing the output on the client side.
+ *
+ * @author Dirk Schnelle-Walka
+ * @version $Revision: 2129 $
+ * @since 0.6
+ *
+ * <p>
+ * Copyright &copy; 2007-2008 JVoiceXML group - <a
+ * href="http://jvoicexml.sourceforge.net"> http://jvoicexml.sourceforge.net/
+ * </a>
+ * </p>
+ */
+public final class RtpPlayer implements RTPAppIntf {
+    /** RTP port number. */
+    private final int rtpPort;
+
+    /** RTP control port. */
+    private final int rtpcPort;
+
+    /** The RTP session. */
+    private RTPSession session;
+
+    /** The line to play back the data. */
+    private SourceDataLine line;
+
+    /**
+     * Constructs a new object.
+     *
+     * @param rtp
+     *            RTP port.
+     * @param rtpc
+     *            RTP control port.
+     */
+    public RtpPlayer(final int rtp, final int rtpc) {
+        rtpPort = rtp;
+        rtpcPort = rtpc;
+    }
+
+    /**
+     * Opens this player.
+     * @exception IOException
+     *            Error connecting to the RTP server.
+     */
+    public void open() throws IOException {
+        final DatagramSocket rtpSocket = new DatagramSocket(rtpPort);
+        final DatagramSocket rtpcSocket = new DatagramSocket(rtpcPort);
+
+        session = new RTPSession(rtpSocket, rtpcSocket);
+        session.naivePktReception(true);
+        session.registerRTPSession(this, null, null);
+
+        AudioFormat.Encoding encoding =  new AudioFormat.Encoding("PCM_SIGNED");
+        AudioFormat format = new AudioFormat(encoding,((float) 8000.0), 16, 1,
+                2, ((float) 8000.0), false);
+
+        DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+
+        try {
+            line = (SourceDataLine) AudioSystem.getLine(info);
+            line.open(format);
+        } catch (LineUnavailableException e) {
+            throw new IOException(e.getMessage());
+        }
+
+
+        line.start();
+    }
+
+    /**
+     * Stops the current output.
+     */
+    public void close() {
+        session.endSession();
+        line.drain();
+        line.close();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int frameSize(final int size) {
+        return 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void receiveData(final DataFrame frame,
+            final Participant participant) {
+        if (line != null) {
+            final byte[] data = frame.getConcatenatedData();
+            line.write(data, 0, data.length);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void userEvent(final int type, final Participant[] participants) {
+    }
+}
