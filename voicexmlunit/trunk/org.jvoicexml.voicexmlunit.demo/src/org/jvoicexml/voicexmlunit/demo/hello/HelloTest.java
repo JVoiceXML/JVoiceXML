f8cb5ef71aa45a33c5ecd7541e45dca87d07011f
damnagehack <damnagehack@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2012-11-18 22:29:22 +0000

    use voicexmlunit.Voice to get engine

--- voicexmlunit/trunk/org.jvoicexml.voicexmlunit.demo/src/org/jvoicexml/voicexmlunit/demo/hello/HelloTest.java
+++ voicexmlunit/trunk/org.jvoicexml.voicexmlunit.demo/src/org/jvoicexml/voicexmlunit/demo/hello/HelloTest.java
@@ -9,23 +9,28 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import org.jvoicexml.JVoiceXml;
+
 import org.jvoicexml.client.text.TextServer;
 
 import org.jvoicexml.voicexmlunit.Conversation;
 import org.jvoicexml.voicexmlunit.Supervisor;
+import org.jvoicexml.voicexmlunit.Voice;
 
 
 public class HelloTest extends TestCase {
 
+	private JVoiceXml jvxml;
 	private Supervisor supervisor;
 
 	@Before
 	protected void setUp() throws Exception {
 		super.setUp();
 		System.setProperty("java.security.policy","config/jvoicexml.policy");
-				
+
+		final File configuration = new File("config/jndi.properties");
+		jvxml = Voice.lookup(configuration);
 		supervisor = new Supervisor();
-		supervisor.lookupVoice(new File("config/jndi.properties"));
 	}
 
 	@After
@@ -36,13 +41,13 @@ public class HelloTest extends TestCase {
 	@Test
 	public void test() {
 		final TextServer server = new TextServer(4242);
-		final Conversation conversation = supervisor.init(server);
+		final Conversation conversation = supervisor.init(server,jvxml);
 
 		server.start();
 		conversation.addOutput("Hello World!");
 		conversation.addOutput("Goodbye!");
 
-		supervisor.process(new File("rc/helloworld.vxml"));
+		supervisor.SERVER_WAIT = 10000;
+		supervisor.process("rc/helloworld.vxml");
 	}
-
 }
