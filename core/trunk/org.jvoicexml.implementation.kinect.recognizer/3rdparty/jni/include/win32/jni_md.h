f38108cb752ffe145c7b7af18be654ebe07b0ef2
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2012-11-28 16:49:49 +0000

    Upgrade to latest jvm version.

--- core/trunk/org.jvoicexml.implementation.kinect.recognizer/3rdparty/jni/include/win32/jni_md.h
+++ core/trunk/org.jvoicexml.implementation.kinect.recognizer/3rdparty/jni/include/win32/jni_md.h
@@ -1,8 +1,26 @@
 /*
- * @(#)jni_md.h	1.15 05/11/17
+ * Copyright (c) 1996, 1998, Oracle and/or its affiliates. All rights reserved.
+ * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  *
- * Copyright 2006 Sun Microsystems, Inc. All rights reserved.
- * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  */
 
 #ifndef _JAVASOFT_JNI_MD_H_
