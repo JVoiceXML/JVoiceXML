be08f4ddcce6a0af2c55d1ee8181079179ce55c5
damnagehack <damnagehack@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2012-11-28 22:38:13 +0000

    commented code to better understand what should go on behind

--- voicexmlunit/trunk/org.jvoicexml.voicexmlunit.demo/src/org/jvoicexml/voicexmlunit/demo/hello/HelloTest.java
+++ voicexmlunit/trunk/org.jvoicexml.voicexmlunit.demo/src/org/jvoicexml/voicexmlunit/demo/hello/HelloTest.java
@@ -2,14 +2,21 @@ package org.jvoicexml.voicexmlunit.demo.hello;
 
 
 import java.io.File;
+import java.net.URI;
+import java.net.UnknownHostException;
 
 import junit.framework.TestCase;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import org.jvoicexml.JVoiceXml;
+import org.jvoicexml.Session;
+
+import org.jvoicexml.client.text.TextServer;
+import org.jvoicexml.event.ErrorEvent;
 
 import org.jvoicexml.voicexmlunit.Runner;
 import org.jvoicexml.voicexmlunit.Supervisor;
@@ -36,16 +43,39 @@ public class HelloTest extends TestCase {
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
-
+/*
 	@Test
-	public void test() {
-		final Runner runner = new Runner("rc/helloworld.vxml",supervisor);
-		final Conversation conversation = supervisor.init(runner.getServer(),jvxml);
-
+	public void testSimple() {
+		final String path = "rc/helloworld.vxml";
+		
+		final TextServer server = new TextServer(4242);
+		server.start();
+		
+		URI dialog = new File(path).toURI();
+		try {
+			final Session session = jvxml.createSession(server.getConnectionInformation());
+			session.call(dialog);
+			session.waitSessionEnd();
+			session.hangup();			
+		} catch (UnknownHostException | ErrorEvent e) {
+			e.printStackTrace();
+			Assert.fail("Session");
+		} finally {
+			server.stopServer();
+		}
+	}
+*/
+	@Test
+	public void testSupervisor() {		
+		final String path = "rc/helloworld.vxml";
+		
+		final Runner runner = new Runner(path,supervisor);
+		final TextServer server = runner.getServer();
+		final Conversation conversation = supervisor.init(server,jvxml);
+		
 		conversation.addOutput("Hello World!");
 		conversation.addOutput("Goodbye!");
 
-		supervisor.SERVER_WAIT = 10000;
 		runner.run();
 	}
 }
