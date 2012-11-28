dce20b96d8b6b7ed2f488a72cbac5f1305ac826f
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2012-11-28 18:47:09 +0000

    Added a 64bit version.

--- core/trunk/org.jvoicexml.implementation.kinect.recognizer/src/JNIUtils.cpp
+++ core/trunk/org.jvoicexml.implementation.kinect.recognizer/src/JNIUtils.cpp
@@ -4,15 +4,24 @@
 
 #define INITGUID
 #include <guiddef.h>
+//#include <log4cplus/logger.h>
+//#include <log4cplus/loggingmacros.h>
+//#include <log4cplus/configurator.h>
 
 // This is the class ID we expect for the Microsoft Speech recognizer.
 // Other values indicate that we're using a version of sapi.h that is
 // incompatible with this sample.
 DEFINE_GUID(CLSID_ExpectedRecognizer, 0x495648e7, 0xf7ab, 0x4267, 0x8e, 0x0f, 0xca, 0xfb, 0x7a, 0x33, 0xc1, 0x60);
 
+//static log4cplus::Logger logger =
+//    log4cplus::Logger::getInstance(L"org.jvoicexml.kinect.recognizer.JNIUtils");
+
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *jvm, void *reserved)
 {
+	//log4cplus::BasicConfigurator config;
+	//config.configure();
+
     if (CLSID_ExpectedRecognizer != CLSID_SpInprocRecognizer)
     {
         char buffer[1024];
