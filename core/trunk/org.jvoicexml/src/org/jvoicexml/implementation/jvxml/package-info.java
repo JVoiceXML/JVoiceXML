2abc64f2d1eaa2cf4459eb96f92e196dee12851a
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2013-01-11 09:37:39 +0000

    Added state diagram to illustrate the the internal structure of the basic implementation platform.

--- core/trunk/org.jvoicexml/src/org/jvoicexml/implementation/jvxml/package-info.java
+++ core/trunk/org.jvoicexml/src/org/jvoicexml/implementation/jvxml/package-info.java
@@ -1,5 +1,11 @@
 /**
  * Basic implementations for JVoiceXML.
+ * 
+ * <p>
+ * The internal structure for this demo implementation is shown in the following
+ * figure:<br/>
+ * <img src="doc-files/Imlementation-Platform.jpg"/>
+ * </p>
  */
 
 package org.jvoicexml.implementation.jvxml;
