6fa9a8105bd90eeb475aa5d7f32209c7d05928f6
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2013-01-11 09:03:42 +0000

    Included UML diagram of the overall architecture.

--- core/trunk/org.jvoicexml/src/org/jvoicexml/package-info.java
+++ core/trunk/org.jvoicexml/src/org/jvoicexml/package-info.java
@@ -1,6 +1,11 @@
 /**
  * This package contains the user relevant interfaces and classes to
  * interact with the JVoiceXML VoiceXML interpreter.
+ * 
+ * <p>
+ * An overview about the architecture is given in the following figure:<br/>
+ * <img src="doc-files/main-components.jpg"/>
+ * </p>
  */
 package org.jvoicexml;
 
