756b3619a6de37353e87a710d7f1dcb75961fb72
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2013-01-28 09:14:23 +0000

    Style corrections.

--- core/trunk/org.jvoicexml.callmanager.mmi.umundo/src/org/jvoicexml/callmanager/mmi/umundo/MmiReceiver.java
+++ core/trunk/org.jvoicexml.callmanager.mmi.umundo/src/org/jvoicexml/callmanager/mmi/umundo/MmiReceiver.java
@@ -1,3 +1,29 @@
+/*
+ * File:    $HeadURL$
+ * Version: $LastChangedRevision$
+ * Date:    $Date$
+ * Author:  $LastChangedBy$
+ *
+ * JVoiceXML - A free VoiceXML implementation.
+ *
+ * Copyright (C) 2013 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
 package org.jvoicexml.callmanager.mmi.umundo;
 
 import java.util.Collection;
@@ -10,18 +36,31 @@ import org.jvoicexml.mmi.events.protobuf.LifeCycleEvents;
 import org.umundo.core.Message;
 import org.umundo.s11n.ITypedReceiver;
 
-public class MmiReceiver implements ITypedReceiver {
+/**
+ * Receiver for MMI events over umundo.
+ * @author Dirk Schnelle-Walka
+ * @version $LastChangedRevision$
+ * @since 0.7.6
+ */
+public final class MmiReceiver implements ITypedReceiver {
     /** Registered listeners for MMI events. */
     private final Collection<MMIEventListener> listeners;
 
     /** The used protocol adapter. */
     private final String sourceUrl;
 
+    /**
+     * Constructs a new object.
+     * @param source the source URL of this endpoint
+     */
     public MmiReceiver(final String source) {
         listeners = new java.util.ArrayList<MMIEventListener>();
         sourceUrl = source;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void receiveObject(Object object, Message msg) {
         LifeCycleEvents.LifeCycleEvent receivedEvent =
--- core/trunk/org.jvoicexml.callmanager.mmi.umundo/src/org/jvoicexml/callmanager/mmi/umundo/UmundoETLProtocolAdapter.java
+++ core/trunk/org.jvoicexml.callmanager.mmi.umundo/src/org/jvoicexml/callmanager/mmi/umundo/UmundoETLProtocolAdapter.java
@@ -1,6 +1,29 @@
-/**
- * 
+/*
+ * File:    $HeadURL$
+ * Version: $LastChangedRevision$
+ * Date:    $Date$
+ * Author:  $LastChangedBy$
+ *
+ * JVoiceXML - A free VoiceXML implementation.
+ *
+ * Copyright (C) 2013 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
  */
+
 package org.jvoicexml.callmanager.mmi.umundo;
 
 import java.io.IOException;
@@ -18,10 +41,12 @@ import org.umundo.s11n.TypedSubscriber;
 import com.google.protobuf.ExtensionRegistry;
 
 /**
- * @author dirk
- * 
+ * An {@link ETLProtocolAdapter} for umundo.
+ * @author Dirk Schnelle-Walka
+ * @version $LastChangedRevision$
+ * @since 0.7.6
  */
-public class UmundoETLProtocolAdapter implements ETLProtocolAdapter {
+public final class UmundoETLProtocolAdapter implements ETLProtocolAdapter {
     /** Logger instance. */
     private static final Logger LOGGER = Logger
             .getLogger(SocketETLProtocolAdapter.class);
