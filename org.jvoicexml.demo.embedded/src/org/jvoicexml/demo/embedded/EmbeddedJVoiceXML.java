a69ab164c8833cd613927cf59348f7cf41c5028d
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2013-02-21 10:22:33 +0000

    reorg: moved demo 0.7.5.GA tags

---
+++ org.jvoicexml.demo.embedded/src/org/jvoicexml/demo/embedded/EmbeddedJVoiceXML.java
@@ -0,0 +1,122 @@
+/*
+ * File:    $HeadURL$
+ * Version: $LastChangedRevision$
+ * Date:    $Date$
+ * Author:  $LastChangedBy$
+ *
+ * JVoiceXML - A free VoiceXML implementation.
+ *
+ * Copyright (C) 2011 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package org.jvoicexml.demo.embedded;
+
+import java.io.File;
+import java.net.URI;
+
+import org.apache.log4j.Logger;
+import org.jvoicexml.ConnectionInformation;
+import org.jvoicexml.JVoiceXmlMain;
+import org.jvoicexml.JVoiceXmlMainListener;
+import org.jvoicexml.Session;
+import org.jvoicexml.client.BasicConnectionInformation;
+import org.jvoicexml.config.JVoiceXmlConfiguration;
+import org.jvoicexml.event.JVoiceXMLEvent;
+
+/**
+ * Demo to show how JVoiceXML can be launched by other applications.
+ * @author Dirk Schnelle-Walka
+ * @version $Revision$
+ * @since 0.7.5
+ */
+public final class EmbeddedJVoiceXML implements JVoiceXmlMainListener {
+    /** Logger for this class. */
+    private static final Logger LOGGER =
+        Logger.getLogger(EmbeddedJVoiceXML.class);
+
+    /** Reference to JVoiceXML. */
+    private JVoiceXmlMain jvxml;
+
+    /**
+     * Do not create from outside.
+     */
+    private EmbeddedJVoiceXML() {
+        // Specify the location of the config folder.
+        System.setProperty("jvoicexml.config", "../org.jvoicexml/config");
+    }
+
+    /**
+     * Calls the VoiceXML interpreter context to process the given XML document.
+     * 
+     * @param uri
+     *            URI of the first document to load
+     * @exception JVoiceXMLEvent
+     *                error processing the call.
+     * @throws InterruptedException
+     *                error waiting for JVoiceXML
+     */
+    private synchronized void interpretDocument(final URI uri)
+            throws JVoiceXMLEvent, InterruptedException {
+        JVoiceXmlConfiguration config = new JVoiceXmlConfiguration();
+        jvxml = new JVoiceXmlMain(config);
+        jvxml.addListener(this);
+        jvxml.start();
+
+        wait();
+
+        final ConnectionInformation client = new BasicConnectionInformation(
+                "dummy", "jsapi10", "jsapi10");
+        final Session session = jvxml.createSession(client);
+
+        session.call(uri);
+        session.waitSessionEnd();
+        session.hangup();
+    }
+
+    /**
+     * The main method.
+     * 
+     * @param args
+     *            Command line arguments. None expected.
+     */
+    public static void main(final String[] args) {
+        final EmbeddedJVoiceXML demo = new EmbeddedJVoiceXML();
+
+        try {
+            File dialog = new File("hello.vxml");
+            final URI uri = dialog.toURI();
+            demo.interpretDocument(uri);
+        } catch (org.jvoicexml.event.JVoiceXMLEvent e) {
+            LOGGER.error("error processing the document", e);
+        } catch (InterruptedException e) {
+            LOGGER.error("error processing the document", e);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public synchronized void jvxmlStarted() {
+        notifyAll();
+    }
+
+    @Override
+    public void jvxmlTerminated() {
+    }
+}
---
+++ org.jvoicexml.demo.mixedinitiativedemo/src/org/jvoicexml/demo/mixedinitiativedemo/MixedInitiativeDemo.java
@@ -0,0 +1,125 @@
+/*
+ * File:    $HeadURL$
+ * Version: $LastChangedRevision$
+ * Date:    $Date$
+ * Author:  $LastChangedBy$
+ *
+ * JVoiceXML Demo - Demo for the free VoiceXML implementation JVoiceXML
+ *
+ * Copyright (C) 2009-2010 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package org.jvoicexml.demo.mixedinitiativedemo;
+
+import java.io.File;
+import java.net.URI;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+import org.apache.log4j.Logger;
+import org.jvoicexml.JVoiceXml;
+import org.jvoicexml.ConnectionInformation;
+import org.jvoicexml.Session;
+import org.jvoicexml.client.BasicConnectionInformation;
+import org.jvoicexml.event.JVoiceXMLEvent;
+
+/**
+ * Demo implementation to demonstrate mixed initiative.
+ * <p>
+ * Must be run with the system property
+ * <code>-Djava.security.policy=${config}/jvoicexml.policy</code> and
+ * the <code>config</code> folder added to the classpath.
+ * </p>
+ *
+ * @author Dirk Schnelle-Walka
+ * @version $Revision$
+ * @since 0.7.2
+ */
+public final class MixedInitiativeDemo {
+    /** Logger for this class. */
+    private static final Logger LOGGER =
+        Logger.getLogger(MixedInitiativeDemo.class);
+
+    /** The JNDI context. */
+    private Context context;
+
+    /**
+     * Do not create from outside.
+     */
+    private MixedInitiativeDemo() {
+        try {
+            context = new InitialContext();
+        } catch (javax.naming.NamingException ne) {
+            LOGGER.error("error creating initial context", ne);
+
+            context = null;
+        }
+    }
+
+    /**
+     * Call the voicexml interpreter context to process the given xml document.
+     *
+     * @param uri
+     *            uri of the first document to load
+     * @exception JVoiceXMLEvent
+     *                Error processing the call.
+     */
+    private void interpretDocument(final URI uri) throws JVoiceXMLEvent {
+        final JVoiceXml jvxml;
+        try {
+            jvxml = (JVoiceXml) context.lookup("JVoiceXml");
+        } catch (javax.naming.NamingException ne) {
+            LOGGER.error("error obtaining JVoiceXml", ne);
+
+            return;
+        }
+
+        final ConnectionInformation client =
+            new BasicConnectionInformation("dummy", "jsapi10", "jsapi10");
+        final Session session = jvxml.createSession(client);
+
+        session.call(uri);
+
+        session.waitSessionEnd();
+        session.hangup();
+    }
+
+    /**
+     * The main method.
+     *
+     * @param args
+     *            Command line arguments. None expected.
+     */
+    public static void main(final String[] args) {
+        LOGGER.info("Starting mixed initiative demo for JVoiceXML...");
+        LOGGER.info("(c) 2009-2011 by JVoiceXML group - "
+                + "http://jvoicexml.sourceforge.net/");
+        try {
+            final MixedInitiativeDemo demo = new MixedInitiativeDemo();
+            final File file = new File("pizza.vxml");
+            final URI uri = file.toURI();
+            LOGGER.info("interpreting document '" + uri + "'...");
+            demo.interpretDocument(uri);
+        } catch (org.jvoicexml.event.JVoiceXMLEvent e) {
+        	LOGGER.error("error processing the document", e);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+    }
+}
