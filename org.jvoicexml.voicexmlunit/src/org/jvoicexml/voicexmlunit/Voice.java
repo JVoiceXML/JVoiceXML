a0c9331f29d2397b0d09732847dbcfc1cfd5dd81
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2013-04-30 07:08:54 +0000

    Added some more comments. Removed unnecessary code.

--- org.jvoicexml.voicexmlunit/src/org/jvoicexml/voicexmlunit/Voice.java
+++ org.jvoicexml.voicexmlunit/src/org/jvoicexml/voicexmlunit/Voice.java
@@ -1,12 +1,36 @@
+/*
+ * File:    $HeadURL: https://jvoicexml.svn.sourceforge.net/svnroot/jvoicexml/core/trunk/org.jvoicexml/src/org/jvoicexml/client/jndi/JVoiceXmlStub.java $
+ * Version: $LastChangedRevision: 2430 $
+ * Date:    $Date: 2010-12-21 09:21:06 +0100 (Di, 21 Dez 2010) $
+ * Author:  $LastChangedBy: schnelle $
+ *
+ * JVoiceXML - A free VoiceXML implementation.
+ *
+ * Copyright (C) 2012-2013 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
 package org.jvoicexml.voicexmlunit;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-
+import java.io.Reader;
 import java.net.URI;
-import java.net.UnknownHostException;
-
 import java.util.Properties;
 
 import javax.naming.Context;
@@ -20,15 +44,20 @@ import org.jvoicexml.event.ErrorEvent;
 
 /**
  * Voice provides direct access to JVoiceXML.
- * @author thesis
+ * @author Raphael Groner
+ * @author Dirk Schnelle-Walka
  */
 public final class Voice {
-    private File configuration = null;
-    private Context context = null;
-    private JVoiceXml jvxml = null;
-    private Session session = null;
+    private File configuration;
+    private Context context;
+    private JVoiceXml jvxml;
+    private Session session;
 
-    public void setPolicy(String path) {
+    /**
+     * Sets the given policy.
+     * @param path the policy
+     */
+    public void setPolicy(final String path) {
         System.setProperty("java.security.policy", path);
     }
 
@@ -44,8 +73,11 @@ public final class Voice {
     }
 
     /**
-     * Get the JVoiceXML object
-     * @throws IOException 
+     * Retrieves the JVoiceXML object. If JVoiceXML has not been looked
+     * up, calling this method will try to connect to the JVoiceXML server.
+     * @throws IOException
+     *         error looking up JVoiceXml
+     * @see #lookupJVoiceXML()
      */
     public JVoiceXml getJVoiceXml() throws IOException {
         if (jvxml == null) {
@@ -64,12 +96,13 @@ public final class Voice {
                 context = new InitialContext();
             } else {
                 final Properties environment = new Properties();
-                environment.load(new FileReader(configuration));
+                final Reader reader = new FileReader(configuration);
+                environment.load(reader);
                 context = NamingManager.getInitialContext(environment);
             }
             jvxml = (JVoiceXml) context.lookup(JVoiceXml.class.getSimpleName());
-        } catch (javax.naming.NamingException | IOException ne) {
-            throw new IOException("JVoiceXML not found! Is it running?");
+        } catch (javax.naming.NamingException | IOException ex) {
+            throw new IOException("JVoiceXML not found! Is it running?", ex);
         }
     }
 
@@ -84,7 +117,7 @@ public final class Voice {
      * Connects a new Session object with a dialog.
      * 
      * @param connectionInformation
-     *            the conection details of the server object
+     *            the connection details of the server object
      * @param dialog
      *            the dialog to use
      * @throws IOException 
@@ -93,7 +126,8 @@ public final class Voice {
     public void connect(final ConnectionInformation connectionInformation, URI dialog)
             throws IOException {
         try {
-            session = getJVoiceXml().createSession(connectionInformation);
+            final JVoiceXml jvxml = getJVoiceXml();
+            session = jvxml.createSession(connectionInformation);
             session.call(dialog);
             session.waitSessionEnd();
             //session.hangup();
