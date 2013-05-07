0e3963dd4b51eb2036206aba0bbeb12b9db1459a
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2013-05-07 07:54:15 +0000

    Some style corrections.

--- trunk/org.jvoicexml.voicexmlunit/unittests/src/org/jvoicexml/voicexmlunit/TestConversation.java
+++ trunk/org.jvoicexml.voicexmlunit/unittests/src/org/jvoicexml/voicexmlunit/TestConversation.java
@@ -10,62 +10,62 @@ import org.jvoicexml.voicexmlunit.processor.Assert;
 
 public class TestConversation {
 
-	private Supervisor supervisor;
+    private Supervisor supervisor;
 
+    @Before
+    public void setUp() throws Exception {
+        supervisor = new Supervisor();
+    }
 
-	@Before
-	public void setUp() throws Exception {
-		supervisor = new Supervisor();
-	}
+    @After
+    public void tearDown() throws Exception {
+    }
 
-	@After
-	public void tearDown() throws Exception {
-	}
+    @Test
+    public void testConversationAdd() {
+        // 1. one Output
+        Conversation conversation = createConversationForSimpleTest();
+        String expected = "Test1";
+        conversation.addOutput(expected);
+        // Assert.assertStatements(1, conversation);
+        Assert.assertEquals(expected, conversation.begin().toString());
 
-	@Test
-	public void testConversationAdd() {
-		// 1. one Output
-		Conversation conversation = createConversationForSimpleTest();
-		String expected = "Test1";
-		conversation.addOutput(expected);
-		//Assert.assertStatements(1, conversation);
-		Assert.assertEquals(expected, conversation.begin().toString());
-		
-		// 2. one Output again
-		conversation = createConversationForSimpleTest();
-		expected = "Test2";
-		conversation.addOutput(expected);
-		//Assert.assertStatements(1, conversation);
-		Assert.assertEquals(expected, conversation.begin().toString());
-		
-		// 3. both Output and Input
-		expected = "Test3";
-		conversation.addInput(expected);
-		//Assert.assertStatements(2, conversation);
-		//Assert.assertEquals(expected, conversation.next().toString()); // TODO
+        // 2. one Output again
+        conversation = createConversationForSimpleTest();
+        expected = "Test2";
+        conversation.addOutput(expected);
+        // Assert.assertStatements(1, conversation);
+        Assert.assertEquals(expected, conversation.begin().toString());
 
-		// 4. conversation gets empty again
-		conversation = createConversationForSimpleTest();
-		//Assert.assertStatements(0, conversation);
-		Assert.assertNull(conversation.next());
-	}
-	
-	@Test
-	public void testConversationNext() {
-		Conversation conversation = createConversationForSimpleTest();
-		conversation.addOutput("begin");		
-		conversation.addOutput("next");
-		
-		Assert.assertEquals(conversation.next().toString(),"begin"); // tricky, eh?
-		Assert.assertEquals(conversation.begin().toString(),"begin");
-		Assert.assertEquals(conversation.next().toString(),"next");
-		Assert.assertNull(conversation.next());
-	}
+        // 3. both Output and Input
+        expected = "Test3";
+        conversation.addInput(expected);
+        // Assert.assertStatements(2, conversation);
+        // Assert.assertEquals(expected, conversation.next().toString()); //
+        // TODO
 
-	
-	private Conversation createConversationForSimpleTest() {
-		Conversation conversation = supervisor.init(null);
-		Assert.assertStatements(0, conversation);
-		return conversation;
-	}
+        // 4. conversation gets empty again
+        conversation = createConversationForSimpleTest();
+        // Assert.assertStatements(0, conversation);
+        Assert.assertNull(conversation.next());
+    }
+
+    @Test
+    public void testConversationNext() {
+        Conversation conversation = createConversationForSimpleTest();
+        conversation.addOutput("begin");
+        conversation.addOutput("next");
+
+        Assert.assertEquals(conversation.next().toString(), "begin"); // tricky,
+                                                                      // eh?
+        Assert.assertEquals(conversation.begin().toString(), "begin");
+        Assert.assertEquals(conversation.next().toString(), "next");
+        Assert.assertNull(conversation.next());
+    }
+
+    private Conversation createConversationForSimpleTest() {
+        Conversation conversation = supervisor.init(null);
+        Assert.assertStatements(0, conversation);
+        return conversation;
+    }
 }
--- trunk/org.jvoicexml.voicexmlunit/unittests/src/org/jvoicexml/voicexmlunit/TestSupervisor.java
+++ trunk/org.jvoicexml.voicexmlunit/unittests/src/org/jvoicexml/voicexmlunit/TestSupervisor.java
@@ -1,28 +1,57 @@
-package org.jvoicexml.voicexmlunit;
+/*
+ * File:    $HeadURL: https://jvoicexml.svn.sourceforge.net/svnroot/jvoicexml/core/trunk/org.jvoicexml/src/org/jvoicexml/client/jndi/JVoiceXmlStub.java $
+ * Version: $LastChangedRevision: 2430 $
+ * Date:    $Date: 2010-12-21 09:21:06 +0100 (Di, 21 Dez 2010) $
+ * Author:  $LastChangedBy: schnelle $
+ *
+ * JVoiceXML - A free VoiceXML implementation.
+ *
+ * Copyright (C) 2012-2013 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
 
+package org.jvoicexml.voicexmlunit;
 
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
 import org.jvoicexml.voicexmlunit.io.Input;
 import org.jvoicexml.voicexmlunit.io.Output;
 import org.jvoicexml.voicexmlunit.processor.Assert;
 import org.jvoicexml.xml.ssml.SsmlDocument;
 
+/**
+ * Test cases for {@link Supervisor}.
+ * @author Raphael Groner
+ *
+ */
 public class TestSupervisor {
 
     private Supervisor supervisor;
 
+    /**
+     * Set up the test environment.
+     * @throws Exception
+     *         set up failed
+     */
     @Before
     public void setUp() throws Exception {
         supervisor = new Supervisor();
-    }
-
-    @After
-    public void tearDown() throws Exception {
     }
 
     @Test
