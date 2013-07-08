1b717376374a0de7b857100c834d0eed8be39f93
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2013-07-08 16:37:03 +0000

    bugfix: MMIReceiver was created after the listeners have been added.

--- org.jvoicexml.callmanager.mmi/src/org/jvoicexml/callmanager/mmi/package-info.java
+++ org.jvoicexml.callmanager.mmi/src/org/jvoicexml/callmanager/mmi/package-info.java
@@ -7,6 +7,26 @@
  * The event and transport layer can be customized by implementing the
  * {@link org.jvoicexml.callmanager.mmi.ETLProtocolAdapter}.
  * </p>
+ * 
+ * <p>
+ * For now it is possible to start a new JVoiceXML session using the
+ * <code>StartRequest</code> and terminate it with a <code>StopRequest</code>.
+ * If the JVoiceXML session terminates, a <code>DoneNotification</code> is
+ * sent. The VoiceXML application can be identified via the
+ * <code>ContentURL</code> attribute of the <code>PrepareRequest</code> or
+ * <code>StartRequest</code>. Alternatively, it is possible to send
+ * VoiceXML snippets in the <code>Content</code> attribute in one of the
+ * previously mentioned messages. The following snippets are currently
+ * supported:
+ * <ul>
+ * <li>strings to have the given text spoken in a prompt</li>
+ * <li>prompt to have the given prompt spoken</li>
+ * <li>field to get input from a VoiceXML document with the given field</li>
+ * </ul>
+ * In order to have this working it is required to deploy
+ * <code>org.jvoicexml.callmanager.mmi.servlet.war</code> to the servlet
+ * container of your choice.
+ * </p>
  */
 package org.jvoicexml.callmanager.mmi;
 
