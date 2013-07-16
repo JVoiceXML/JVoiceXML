d3c599d9d2615fe65b83e4e53c53cb36209d1c29
damnagehack <damnagehack@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2013-07-16 13:08:49 +0000

    handle ConcurrentModificationException

--- trunk/org.jvoicexml.voicexmlunit/unittests/src/org/jvoicexml/voicexmlunit/TestConversation.java
+++ trunk/org.jvoicexml.voicexmlunit/unittests/src/org/jvoicexml/voicexmlunit/TestConversation.java
@@ -26,9 +26,10 @@
 
 package org.jvoicexml.voicexmlunit;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+
+import org.junit.*;
 
 import org.jvoicexml.voicexmlunit.Conversation;
 import org.jvoicexml.voicexmlunit.Supervisor;
@@ -53,26 +54,33 @@ public class TestConversation {
         Conversation conversation = createConversationForSimpleTest();
         String expected = "Test1";
         conversation.addOutput(expected);
-        // Assert.assertStatements(1, conversation);
+        Assert.assertStatements(1, conversation);
         Assert.assertEquals(expected, conversation.begin().toString());
 
         // 2. one Output again
         conversation = createConversationForSimpleTest();
         expected = "Test2";
         conversation.addOutput(expected);
-        // Assert.assertStatements(1, conversation);
+        Assert.assertStatements(1, conversation);
         Assert.assertEquals(expected, conversation.begin().toString());
 
         // 3. both Output and Input
         expected = "Test3";
         conversation.addInput(expected);
-        // Assert.assertStatements(2, conversation);
-        // Assert.assertEquals(expected, conversation.next().toString()); //
-        // TODO
+        Assert.assertStatements(2, conversation);
+
+        // TODO: change the following behaviour for synchronous test
+        try {
+                conversation.next(); // fails always
+                Assert.fail("Expected ConcurrentModificationException");
+        } catch (ConcurrentModificationException ignore) {
+        }
+        conversation.begin(); // prevents ConcurrentModificationException
+        Assert.assertEquals(expected, conversation.next().toString());
 
         // 4. conversation gets empty again
         conversation = createConversationForSimpleTest();
-        // Assert.assertStatements(0, conversation);
+        Assert.assertStatements(0, conversation);
         Assert.assertNull(conversation.next());
     }
 
@@ -82,8 +90,7 @@ public class TestConversation {
         conversation.addOutput("begin");
         conversation.addOutput("next");
 
-        Assert.assertEquals(conversation.next().toString(), "begin"); // tricky,
-                                                                      // eh?
+        Assert.assertEquals(conversation.next().toString(), "begin");
         Assert.assertEquals(conversation.begin().toString(), "begin");
         Assert.assertEquals(conversation.next().toString(), "next");
         Assert.assertNull(conversation.next());
