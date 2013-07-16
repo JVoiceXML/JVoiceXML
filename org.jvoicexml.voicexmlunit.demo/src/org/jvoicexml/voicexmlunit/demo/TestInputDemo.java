900a7decf9a2c76ec2091ef998d64413a899228b
damnagehack <damnagehack@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2013-07-16 19:23:58 +0000

    deprecated warnings cleanup

--- org.jvoicexml.voicexmlunit.demo/src/org/jvoicexml/voicexmlunit/demo/TestInputDemo.java
+++ org.jvoicexml.voicexmlunit.demo/src/org/jvoicexml/voicexmlunit/demo/TestInputDemo.java
@@ -11,6 +11,8 @@ import org.jvoicexml.voicexmlunit.Call;
 import org.jvoicexml.voicexmlunit.Conversation;
 import org.jvoicexml.voicexmlunit.Supervisor;
 import org.jvoicexml.voicexmlunit.Voice;
+import org.jvoicexml.voicexmlunit.io.Output;
+import org.jvoicexml.voicexmlunit.io.Input;
 
 public class TestInputDemo {
   private Call call;
@@ -37,8 +39,8 @@ public class TestInputDemo {
 
   private void createConversation(String answer) {
     Conversation conversation = supervisor.init(call);
-    conversation.addOutput("Do you like this example?");
-    conversation.addInput(answer);
-    conversation.addOutput("You like this example.");
+    conversation.add(new Output("Do you like this example?"));
+    conversation.add(new Input(answer));
+    conversation.add(new Output("You like this example."));
   }
 }
