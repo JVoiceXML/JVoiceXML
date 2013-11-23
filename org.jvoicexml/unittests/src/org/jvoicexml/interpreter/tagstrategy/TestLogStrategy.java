5a73a17dff90ac62988b9271ecbb3088c4cd5027
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2013-11-23 17:53:42 +0000

    added a log test with a function call.

--- org.jvoicexml/unittests/src/org/jvoicexml/interpreter/tagstrategy/TestLogStrategy.java
+++ org.jvoicexml/unittests/src/org/jvoicexml/interpreter/tagstrategy/TestLogStrategy.java
@@ -6,7 +6,7 @@
  *
  * JVoiceXML - A free VoiceXML implementation.
  *
- * Copyright (C) 2007-2010 JVoiceXML group - http://jvoicexml.sourceforge.net
+ * Copyright (C) 2013 JVoiceXML group - http://jvoicexml.sourceforge.net
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Library General Public
@@ -34,6 +34,9 @@ import org.jvoicexml.interpreter.ScriptingEngine;
 import org.jvoicexml.mock.TestAppender;
 import org.jvoicexml.xml.vxml.Block;
 import org.jvoicexml.xml.vxml.Log;
+import org.jvoicexml.xml.vxml.Script;
+import org.jvoicexml.xml.vxml.VoiceXmlDocument;
+import org.jvoicexml.xml.vxml.Vxml;
 
 /**
  * This class provides a test case for the {@link LogStrategy}.
@@ -104,6 +107,43 @@ public final class TestLogStrategy
         }
         final String message = TestAppender.TEST_PREFIX
                 + "actor is Horst Buchholz"; 
+        Assert.assertTrue("message not found in appender",
+                TestAppender.containsMessage(message));
+    }
+
+    /**
+     * Test method for
+     * {@link org.jvoicexml.interpreter.tagstrategy.LogStrategy#execute(org.jvoicexml.interpreter.VoiceXmlInterpreterContext, org.jvoicexml.interpreter.VoiceXmlInterpreter, org.jvoicexml.interpreter.FormInterpretationAlgorithm, org.jvoicexml.interpreter.FormItem, org.jvoicexml.xml.VoiceXmlNode)}.
+     * @exception Exception
+     *            test failed
+     */
+    @Test
+    public void testExecuteExprFunctionCall() throws Exception {
+        final String scriptFactorial = "var a = 42;"
+                + "function factorial(n)"
+                + "{"
+                + "return (n <= 1)? 1 : n * factorial(n-1);"
+                + "}";
+        final VoiceXmlDocument doc = createDocument();
+        final Vxml vxml = doc.getVxml();
+        final Script script = vxml.appendChild(Script.class);
+        script.addCdata(scriptFactorial);
+
+        final Block block = createBlock();
+        final Log log = block.appendChild(Log.class);
+        final String expr = "'" + TestAppender.TEST_PREFIX
+                + "6 factorial is ' + factorial(6)";
+        log.setExpr(expr);
+
+        LogStrategy strategy = new LogStrategy();
+        try {
+            executeTagStrategy(script, new ScriptStrategy());
+            executeTagStrategy(log, strategy);
+        } catch (JVoiceXMLEvent e) {
+            Assert.fail(e.getMessage());
+        }
+        final String message = TestAppender.TEST_PREFIX
+                + "6 factorial is 720"; 
         Assert.assertTrue("message not found in appender",
                 TestAppender.containsMessage(message));
     }
