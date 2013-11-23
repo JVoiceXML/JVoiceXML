062a0ba20936413631178a966b2a2e79f9a3ac98
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2013-11-23 20:35:38 +0000

    set keywords.

--- trunk/org.jvoicexml.callmanager.sip/src/org/jvoicexml/callmanager/sip/JVoiceXmlUserAgent.java
+++ trunk/org.jvoicexml.callmanager.sip/src/org/jvoicexml/callmanager/sip/JVoiceXmlUserAgent.java
@@ -1,8 +1,8 @@
 /*
- * File:    $HeadURL:  $
- * Version: $LastChangedRevision: 643 $
- * Date:    $Date: $
- * Author:  $LastChangedBy: $
+ * File:    $HeadURL$
+ * Version: $LastChangedRevision$
+ * Date:    $Date$
+ * Author:  $LastChangedBy$
  *
  * JVoiceXML - A free VoiceXML implementation.
  *
@@ -72,7 +72,7 @@ import org.apache.log4j.Logger;
  * A SIP user agent implementation.
  * 
  * @author Dirk Schnelle-Walka
- * @version $Revision: $
+ * @version $Revision$
  * @since 0.7.6
  */
 public class JVoiceXmlUserAgent {
--- trunk/org.jvoicexml.callmanager.sip/src/org/jvoicexml/callmanager/sip/SipTerminal.java
+++ trunk/org.jvoicexml.callmanager.sip/src/org/jvoicexml/callmanager/sip/SipTerminal.java
@@ -1,8 +1,8 @@
 /*
- * File:    $HeadURL:  $
- * Version: $LastChangedRevision: 643 $
- * Date:    $Date: $
- * Author:  $LastChangedBy: $
+ * File:    $HeadURL$
+ * Version: $LastChangedRevision$
+ * Date:    $Date$
+ * Author:  $LastChangedBy$
  *
  * JVoiceXML - A free VoiceXML implementation.
  *
@@ -39,7 +39,7 @@ import org.jvoicexml.callmanager.TerminalListener;
 /**
  * Implementation of a terminal for the SIP callmanager
  * @author Dirk Schnelle-Walka
- * @version $Revision: $
+ * @version $Revision$
  * @since 0.7.6
  */
 public final class SipTerminal
