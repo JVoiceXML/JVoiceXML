18a732a42b3a83609ffcbf93d1748532e35c706d
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2013-11-23 20:44:36 +0000

    set keywords.

--- org.jvoicexml.implementation.marc/src/org/jvoicexml/implementation/marc/MarcClient.java
+++ org.jvoicexml.implementation.marc/src/org/jvoicexml/implementation/marc/MarcClient.java
@@ -1,8 +1,8 @@
 /*
- * File:    $HeadURL:  $
- * Version: $LastChangedRevision: 643 $
- * Date:    $Date: $
- * Author:  $LastChangedBy: $
+ * File:    $HeadURL$
+ * Version: $LastChangedRevision$
+ * Date:    $Date$
+ * Author:  $LastChangedBy$
  *
  * JVoiceXML - A free VoiceXML implementation.
  *
@@ -30,7 +30,7 @@ import java.io.IOException;
 /**
  * A client that is able to communicate with MARC.
  * @author Dirk Schnelle-Walka
- * @version $Revision: $
+ * @version $Revision$
  * @since 0.7.5
  */
 public interface MarcClient {
