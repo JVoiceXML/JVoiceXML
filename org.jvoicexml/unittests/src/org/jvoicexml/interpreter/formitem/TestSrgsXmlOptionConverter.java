3ba34c1ef8814a21b6f38c68a383b012813ae8f7
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2013-11-23 20:28:34 +0000

    set keywords.

--- org.jvoicexml/unittests/src/org/jvoicexml/interpreter/formitem/TestSrgsXmlOptionConverter.java
+++ org.jvoicexml/unittests/src/org/jvoicexml/interpreter/formitem/TestSrgsXmlOptionConverter.java
@@ -1,8 +1,8 @@
 /*
- * File:    $HeadURL:  $
- * Version: $LastChangedRevision: 643 $
- * Date:    $Date: $
- * Author:  $LastChangedBy: $
+ * File:    $HeadURL$
+ * Version: $LastChangedRevision$
+ * Date:    $Date$
+ * Author:  $LastChangedBy$
  *
  * JVoiceXML - A free VoiceXML implementation.
  *
@@ -44,7 +44,7 @@ import org.jvoicexml.xml.vxml.Vxml;
 /**
  * Test cases for {@link org.jvoicexml.interpreter.formitem.SrgsXmlOptionConverter}.
  * @author Dirk Schnelle-Walka
- * @version $Revision: $
+ * @version $Revision$
  * @since 0.7.6
  */
 public final class TestSrgsXmlOptionConverter {
--- org.jvoicexml/unittests/src/org/jvoicexml/mock/implementation/MockSpokenInputListener.java
+++ org.jvoicexml/unittests/src/org/jvoicexml/mock/implementation/MockSpokenInputListener.java
@@ -1,8 +1,8 @@
 /*
- * File:    $HeadURL:  $
- * Version: $LastChangedRevision: 643 $
- * Date:    $Date: $
- * Author:  $LastChangedBy: $
+ * File:    $HeadURL$
+ * Version: $LastChangedRevision$
+ * Date:    $Date$
+ * Author:  $LastChangedBy$
  *
  * JVoiceXML - A free VoiceXML implementation.
  *
@@ -36,7 +36,7 @@ import org.jvoicexml.implementation.SpokenInputListener;
 /**
  * Dummy implementation of a {@link SpokenInputListener}.
  * @author Dirk Schnelle-Walka
- * @version $Revision: $
+ * @version $Revision$
  * @since 0.7.6
  */
 public class MockSpokenInputListener implements SpokenInputListener {
