eb1a4f506502864d12f4de9b8e7af1e029e19dab
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2013-11-23 20:32:07 +0000

    set keywords.

--- org.jvoicexml.callmanager.mmi/src/org/jvoicexml/callmanager/mmi/DecoratedMMIEvent.java
+++ org.jvoicexml.callmanager.mmi/src/org/jvoicexml/callmanager/mmi/DecoratedMMIEvent.java
@@ -1,8 +1,8 @@
 /*
- * File:    $HeadURL:  $
- * Version: $LastChangedRevision: 643 $
- * Date:    $Date: $
- * Author:  $LastChangedBy: $
+ * File:    $HeadURL$
+ * Version: $LastChangedRevision$
+ * Date:    $Date$
+ * Author:  $LastChangedBy$
  *
  * JVoiceXML - A free VoiceXML implementation.
  *
@@ -33,7 +33,7 @@ import org.jvoicexml.mmi.events.LifeCycleEvent;
  * this will be the instance that received the request and should send a
  * response.
  * @author Dirk Schnelle-Walka
- * @version $Revision: $
+ * @version $Revision$
  * @since 0.7.6
  */
 
--- org.jvoicexml.callmanager.mmi/src/org/jvoicexml/callmanager/mmi/MMIContext.java
+++ org.jvoicexml.callmanager.mmi/src/org/jvoicexml/callmanager/mmi/MMIContext.java
@@ -1,8 +1,8 @@
 /*
- * File:    $HeadURL:  $
- * Version: $LastChangedRevision: 643 $
- * Date:    $Date: $
- * Author:  $LastChangedBy: $
+ * File:    $HeadURL$
+ * Version: $LastChangedRevision$
+ * Date:    $Date$
+ * Author:  $LastChangedBy$
  *
  * JVoiceXML - A free VoiceXML implementation.
  *
@@ -33,7 +33,7 @@ import org.jvoicexml.Session;
 /**
  * Associated a communication channel object with MMI request identifiers.
  * @author Dirk Schnelle-walka
- * @version $Revision: $
+ * @version $Revision$
  * @since 0.7.6
  */
 public final class MMIContext {
