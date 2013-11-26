caa3237d3d0e2be1526c3b3bebf7448c149e56e5
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2013-11-26 10:24:13 +0000

    Added some javadoc comments.

--- org.jvoicexml.voicexmlunit/unittests/src/org/jvoicexml/voicexmlunit/io/TestOutput.java
+++ org.jvoicexml.voicexmlunit/unittests/src/org/jvoicexml/voicexmlunit/io/TestOutput.java
@@ -29,21 +29,17 @@ package org.jvoicexml.voicexmlunit.io;
 
 import java.io.IOException;
 
-import java.lang.AssertionError;
-
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.junit.Assert;
 import org.junit.Before;
-import org.junit.ComparisonFailure;
 import org.junit.Test;
-
 import org.jvoicexml.xml.ssml.SsmlDocument;
-
 import org.xml.sax.SAXException;
 
 /**
- * @author thesis
+ * Unit tests for {@link Output}.
+ * @author Raphael Groner
+ * @author Dirk Schnelle-Walka
  *
  */
 public class TestOutput implements AbstractTestAssertion {
