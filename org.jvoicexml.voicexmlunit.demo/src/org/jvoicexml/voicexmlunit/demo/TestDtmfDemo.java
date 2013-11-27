2d942d3ae514bf2452c4f72eac3f02acd3f5d1cf
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2013-11-27 21:56:26 +0000

    Made DTMF demo work again.

--- org.jvoicexml.voicexmlunit.demo/src/org/jvoicexml/voicexmlunit/demo/TestDtmfDemo.java
+++ org.jvoicexml.voicexmlunit.demo/src/org/jvoicexml/voicexmlunit/demo/TestDtmfDemo.java
@@ -3,39 +3,67 @@ package org.jvoicexml.voicexmlunit.demo;
 import java.io.File;
 import java.net.URI;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.jvoicexml.event.JVoiceXMLEvent;
 import org.jvoicexml.voicexmlunit.Call;
 
-public class TestDtmfDemo {
+/**
+ * A demo that tests DTMF input.
+ * @author Raphael Groner
+ * @author Dirk Schnelle-Walka
+ * @since 0.7.6
+ *
+ */
+public final class TestDtmfDemo {
+    /** The call to JVoiceXML. */
     private Call call;
     /** URI of the application to call. */
     private URI uri;
 
+    /**
+     * Set up the test environment.
+     * @throws Exception
+     *        error setting up the test environment
+     */
     @Before
     public void setUp() throws Exception {
         uri = new File("etc/dtmf.vxml").toURI();
         call = new Call();
     }
 
+    /**
+     * Tear down the test environment.
+     * @exception JVoiceXMLEvent
+     *            error tearing down
+     */
+    @After
+    public void tearDown() throws JVoiceXMLEvent {
+        call.hangup();
+    }
+
+    /**
+     * Runs a test with the option '1'.
+     */
     @Test
     public void testInputYes() {
-        createConversation('1');
-//        supervisor.process(uri);
+        call.call(uri);
+        call.hears(
+               "Do you like this example? Please enter 1 for yes or 2 for no");
+        call.enter("1");
+        call.hears("You like this example.");
     }
 
-    @Test(expected = AssertionError.class)
-    public void testInputNoFail() {
-        createConversation('2');
-//        supervisor.process(uri);
-    }
-
-    private void createConversation(char answer) {
-//        Conversation conversation = supervisor.init(call);
-//        conversation
-//                .add(new Output(
-//                        "Do you like this example? Please enter 1 for yes or 2 for no"));
-//        conversation.add(new Dtmf(answer));
-//        conversation.add(new Output("You like this example."));
+    /**
+     * Runs a test with the option '2'.
+     */
+    @Test
+    public void testInputNo() {
+        call.call(uri);
+        call.hears(
+               "Do you like this example? Please enter 1 for yes or 2 for no");
+        call.enter("2");
+        call.hears("You do not like this example.");
     }
 }
