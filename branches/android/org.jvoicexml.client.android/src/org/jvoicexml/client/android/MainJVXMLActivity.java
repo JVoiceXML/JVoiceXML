f9870204a9c174c2b838b9ec615f8f7343f41f0a
macinos <macinos@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2013-12-13 21:55:54 +0000

    Correct ssml document text output fix and inclusion of chardet.jar for documents with grammar.

--- branches/android/org.jvoicexml.client.android/src/org/jvoicexml/client/android/MainJVXMLActivity.java
+++ branches/android/org.jvoicexml.client.android/src/org/jvoicexml/client/android/MainJVXMLActivity.java
@@ -14,6 +14,7 @@ import org.jvoicexml.Session;
 import org.jvoicexml.client.text.TextListener;
 import org.jvoicexml.client.text.TextServer;
 import org.jvoicexml.event.ErrorEvent;
+import org.jvoicexml.xml.ssml.Speak;
 import org.jvoicexml.xml.ssml.SsmlDocument;
 import org.jvoicexml.client.android.R;
 
@@ -34,275 +35,275 @@ import android.widget.TextView;
  * @author macinos
  * 
  */
-public class MainJVXMLActivity extends Activity implements TextListener,
-		JVoiceXmlMainListener {
+public class MainJVXMLActivity extends Activity
+        implements TextListener, JVoiceXmlMainListener {
 
-	private static Object lock = new Object();
-	private Object jvxmlMonitor = new Object();
-	private static TextServer server;
-	private JVoiceXml jvxml;
-	private Session session;
-	private boolean connected;
-	private boolean serverStarted;
-	private static File documentVXML;
+    private static Object lock = new Object();
+    private Object jvxmlMonitor = new Object();
+    private static TextServer server;
+    private JVoiceXml jvxml;
+    private Session session;
+    private boolean connected;
+    private boolean serverStarted;
+    private static File documentVXML;
 
-	private EditText textIn;
-	private static TextView textOut;
-	private Button startButton;
-	private Button openVXMLButton;
+    private EditText textIn;
+    private static TextView textOut;
+    private Button startButton;
+    private Button openVXMLButton;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main_jvxml);
-		textIn = (EditText) findViewById(R.id.textIn);
-		textOut = (TextView) findViewById(R.id.textOut);
-		startButton = (Button) findViewById(R.id.startButton);
-		openVXMLButton = (Button) findViewById(R.id.openVXMLButton);
-		org.apache.log4j.BasicConfigurator.configure();
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_jvxml);
+        textIn = (EditText) findViewById(R.id.textIn);
+        textOut = (TextView) findViewById(R.id.textOut);
+        startButton = (Button) findViewById(R.id.startButton);
+        openVXMLButton = (Button) findViewById(R.id.openVXMLButton);
+        org.apache.log4j.BasicConfigurator.configure();
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_main_jvxml, menu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main_jvxml, menu);
+        return true;
+    }
 
-	@Override
-	public void connected(InetSocketAddress arg0) {
-		connected = true;
-	}
+    @Override
+    public void connected(InetSocketAddress arg0) {
+        connected = true;
+    }
 
-	@Override
-	public void disconnected() {
-		session.hangup();
-		connected = false;
-		runOnUiThread(new Runnable() {
-			public void run() {
-				startButton.setEnabled(true);
-				startButton.requestFocus();
-				textIn.setEnabled(false);
-				textOut.append("--Session ended.\n");
-			}
-		});
-	}
+    @Override
+    public void disconnected() {
+        session.hangup();
+        connected = false;
+        runOnUiThread(new Runnable() {
+            public void run() {
+                startButton.setEnabled(true);
+                startButton.requestFocus();
+                textIn.setEnabled(false);
+                textOut.append("--Session ended.\n");
+            }
+        });
+    }
 
-	@Override
-	public void outputSsml(final SsmlDocument doc) {
-	    runOnUiThread(new Runnable() {
-                public void run() {
-			textOut.append(doc.toString() + "\n");
-		}
-	    });
-	}
+    @Override
+    public void outputSsml(SsmlDocument doc) {
+        Speak speak = doc.getSpeak();
+        final String text = speak.getTextContent();
+        runOnUiThread(new Runnable() {
+            public void run() {
+                synchronized (textOut) {                   
+                    textOut.append(text + "\n");
+                }
+            }
+        });
 
-	/**
-	 * Used outside this class.
-	 * 
-	 * @param outTextExt
-	 */
-	public static void outputTextExternal(String outTextExt) {
-		synchronized (textOut) {
-			textOut.append(outTextExt + "\n");
-		}
-	}
+    }
 
-	@Override
-	public void started() {
-		synchronized (lock) {
-			lock.notifyAll();
-		}
-		serverStarted = true;
-		// manipulation with UI elements has to be done on the original (UI)
-		// thread
-		runOnUiThread(new Runnable() {
-			public void run() {
-				textIn.setEnabled(true);
-				textIn.requestFocus();
-				startButton.setEnabled(false);
-				textOut.append("--Session started.\n");
-			}
-		});
+    /**
+     * Used outside this class.
+     * 
+     * @param outTextExt
+     */
+    public static void outputTextExternal(String outTextExt) {
+        synchronized (textOut) {
+            textOut.append(outTextExt + "\n");
+        }
+    }
 
-	}
+    @Override
+    public void started() {
+        synchronized (lock) {
+            lock.notifyAll();
+        }
+        serverStarted = true;
+        // manipulation with UI elements has to be done on the original (UI)
+        // thread
+        runOnUiThread(new Runnable() {
+            public void run() {
+                textIn.setEnabled(true);
+                textIn.requestFocus();
+                startButton.setEnabled(false);
+                textOut.append("--Session started.\n");
+            }
+        });
 
-	/**
-	 * Sends user input to the interpreter. (Not used yet)
-	 */
-	private void inputText() {
-		if (connected) {
-			try {
-				synchronized (textOut) {
-					textOut.append(textIn.getText() + "\n");
-				}
-				server.sendInput(textIn.getText().toString());
-			} catch (IOException ex) {
-				ex.printStackTrace();
-			}
-		}
-		textIn.setText("");
-	}
+    }
 
-	/**
-	 * Set VXML document for this GUI.
-	 * 
-	 * @param documentVXML
-	 */
-	public static void setDocumentVXML(File newDocumentVXML) {
-		documentVXML = newDocumentVXML;
-	}
+    /**
+     * Sends user input to the interpreter. (Not used yet)
+     */
+    private void inputText() {
+        if (connected) {
+            try {
+                synchronized (textOut) {
+                    textOut.append(textIn.getText() + "\n");
+                }
+                server.sendInput(textIn.getText().toString());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        textIn.setText("");
+    }
 
-	public Button getStartButton() {
-		return startButton;
-	}
+    /**
+     * Set VXML document for this GUI.
+     * 
+     * @param documentVXML
+     */
+    public static void setDocumentVXML(File newDocumentVXML) {
+        documentVXML = newDocumentVXML;
+    }
 
-	/**
-	 * Starts JVXML session to process the VXML document.
-	 */
-	public void startSession() {
-		AndroidTextConfiguration config = new AndroidTextConfiguration();
-		JVoiceXmlMain jvxmlmain = new JVoiceXmlMain(config);
-		jvxmlmain.addListener(this);
-		jvxmlmain.start();
-		synchronized (jvxmlMonitor) {
-			try {
-				jvxmlMonitor.wait();
-			} catch (InterruptedException e) {
-				textOut.append("jvxml not started:" + e.getMessage());
-				return;
-			}
-		}
+    public Button getStartButton() {
+        return startButton;
+    }
 
-		jvxml = jvxmlmain;
+    /**
+     * Starts JVXML session to process the VXML document.
+     */
+    public void startSession() {
+        AndroidTextConfiguration config = new AndroidTextConfiguration();
+        JVoiceXmlMain jvxmlmain = new JVoiceXmlMain(config);
+        jvxmlmain.addListener(this);
+        jvxmlmain.start();
+        synchronized (jvxmlMonitor) {
+            try {
+                jvxmlMonitor.wait();
+            } catch (InterruptedException e) {
+                textOut.append("jvxml not started:" + e.getMessage());
+                return;
+            }
+        }
 
-		if (jvxml == null) {
-			textOut.append("--JVXML not found!\n");
-			startButton.setEnabled(true);
-			return;
-		}
+        jvxml = jvxmlmain;
 
-		if (!serverStarted) {
-			server = new TextServer(4242);
-			server.addTextListener(this);
-			server.start();
-			synchronized (lock) {
-				try {
-					lock.wait();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		} else {
-			startButton.setEnabled(false);
-			textIn.setEnabled(true);
-			textIn.requestFocus();
-			textOut.append("--Session started.\n");
-		}
+        if (jvxml == null) {
+            textOut.append("--JVXML not found!\n");
+            startButton.setEnabled(true);
+            return;
+        }
 
-		// do interpretation on separate thread
-		new ConnectOnBackground().execute();
+        if (!serverStarted) {
+            server = new TextServer(4242);
+            server.addTextListener(this);
+            server.start();
+            synchronized (lock) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            startButton.setEnabled(false);
+            textIn.setEnabled(true);
+            textIn.requestFocus();
+            textOut.append("--Session started.\n");
+        }
 
-	}
+        // do interpretation on separate thread
+        new ConnectOnBackground().execute();
 
-	/**
-	 * Class for running time expensive methods from startSession() on
-	 * background thread (Android standard).
-	 * 
-	 * @author macinos
-	 * 
-	 */
-	private class ConnectOnBackground extends AsyncTask<Void, Void, Void> {
+    }
 
-		@Override
-		protected Void doInBackground(Void... arg0) {
-			final ConnectionInformation info;
-			final URI uri;
+    /**
+     * Class for running time expensive methods from startSession() on
+     * background thread (Android standard).
+     * 
+     * @author macinos
+     * 
+     */
+    private class ConnectOnBackground extends AsyncTask<Void, Void, Void> {
 
-			if (documentVXML != null && documentVXML.canRead()) {
-				try {
-					info = server.getConnectionInformation();
-					session = jvxml.createSession(info);
-					uri = documentVXML.toURI();
-					session.call(uri);
-					session.waitSessionEnd();
-					session.hangup();
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            final ConnectionInformation info;
+            final URI uri;
 
-				} catch (UnknownHostException uhex) {
-					uhex.printStackTrace();
-				} catch (ErrorEvent eex) {
-					eex.printStackTrace();
-				}
-			} else {
-				startButton.setEnabled(true);
-			}
-			return null;
-		}
+            if (documentVXML != null && documentVXML.canRead()) {
+                try {
+                    info = server.getConnectionInformation();
+                    session = jvxml.createSession(info);
+                    uri = documentVXML.toURI();
+                    session.call(uri);
+                    session.waitSessionEnd();
+                    session.hangup();
 
-		protected void onPostExecute(Void result) {
-			// Post results to main thread
-		}
-	}
+                } catch (UnknownHostException uhex) {
+                    uhex.printStackTrace();
+                } catch (ErrorEvent eex) {
+                    eex.printStackTrace();
+                }
+            } else {
+                startButton.setEnabled(true);
+            }
+            return null;
+        }
 
-	/**
-	 * VXML Opening dialog after clicking Open VXML button.
-	 * 
-	 * @param view
-	 */
-	public void openVXML(View view) {
-		Intent intent = new Intent(this, OpenVXMLActivity.class);
-		startActivity(intent);
-	}
+        protected void onPostExecute(Void result) {
+            // Post results to main thread
+        }
+    }
 
-	/**
-	 * Method run when pressing the Start button in the interface.
-	 * 
-	 * @param view
-	 */
-	public void startInterpreter(View view) {
-		startSession();
-	}
+    /**
+     * VXML Opening dialog after clicking Open VXML button.
+     * 
+     * @param view
+     */
+    public void openVXML(View view) {
+        Intent intent = new Intent(this, OpenVXMLActivity.class);
+        startActivity(intent);
+    }
 
-	@Override
-	public void expectingInput() {
-		// TODO Auto-generated method stub
+    /**
+     * Method run when pressing the Start button in the interface.
+     * 
+     * @param view
+     */
+    public void startInterpreter(View view) {
+        startSession();
+    }
 
-	}
+    @Override
+    public void expectingInput() {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void inputClosed() {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    @Override
+    public void inputClosed() {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void jvxmlStartupError(Throwable e) {
-		System.out.println("jvxml startup error:" + e.getMessage());
-		runOnUiThread(new Runnable() {
-			public void run() {
-				textOut.append("jvxml startup error");
-			}
-		});
-		synchronized (jvxmlMonitor) {
-			jvxmlMonitor.notifyAll();
-		}
-	}
+    }
 
-	@Override
-	public void jvxmlStarted() {
-		runOnUiThread(new Runnable() {
-			public void run() {
-				textOut.append("jvxmlStarted");
-			}
-		});
-		synchronized (jvxmlMonitor) {
-			jvxmlMonitor.notifyAll();
-		}
-	}
+    @Override
+    public void jvxmlStartupError(Throwable e) {
+        System.out.println("jvxml startup error:" + e.getMessage());
+        runOnUiThread(new Runnable() {
+            public void run() {
+                textOut.append("jvxml startup error");
+            }
+        });
+        synchronized (jvxmlMonitor) {
+            jvxmlMonitor.notifyAll();
+        }
+    }
 
-	@Override
-	public void jvxmlTerminated() {
-		// TODO Auto-generated method stub
+    @Override
+    public void jvxmlStarted() {
+        synchronized (jvxmlMonitor) {
+            jvxmlMonitor.notifyAll();
+        }
+    }
 
-	}
+    @Override
+    public void jvxmlTerminated() {
+        // TODO Auto-generated method stub
+
+    }
 
 }
