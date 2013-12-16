1289189846836a44276ecfe5d4461036b10eaa3c
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2013-12-16 08:46:03 +0000

    preparation for a MockCall object.

--- trunk/org.jvoicexml.voicexmlunit.demo/src/org/jvoicexml/voicexmlunit/demo/TestHelloDemo.java
+++ trunk/org.jvoicexml.voicexmlunit.demo/src/org/jvoicexml/voicexmlunit/demo/TestHelloDemo.java
@@ -34,6 +34,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.jvoicexml.event.JVoiceXMLEvent;
 import org.jvoicexml.voicexmlunit.Call;
+import org.jvoicexml.voicexmlunit.TextCall;
 
 /**
  * A demo that tests the venerable hello world.
@@ -56,7 +57,7 @@ public final class TestHelloDemo {
     @Before
     public void setUp() throws Exception {
         uri = new File("etc/helloworld.vxml").toURI();
-        call = new Call();
+        call = new TextCall();
     }
 
     /**
