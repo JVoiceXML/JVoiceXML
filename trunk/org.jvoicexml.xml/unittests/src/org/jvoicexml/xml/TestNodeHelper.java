72bc62a06c8a15ba5f124b856b2b0a4d3c10251b
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2014-03-05 12:15:19 +0000

    enhanced test ot check if a space is added in case of trimmed text.

--- trunk/org.jvoicexml.xml/unittests/src/org/jvoicexml/xml/TestNodeHelper.java
+++ trunk/org.jvoicexml.xml/unittests/src/org/jvoicexml/xml/TestNodeHelper.java
@@ -6,7 +6,7 @@
  *
  * JVoiceXML - A free VoiceXML implementation.
  *
- * Copyright (C) 2010 JVoiceXML group - http://jvoicexml.sourceforge.net
+ * Copyright (C) 2010-2014 JVoiceXML group - http://jvoicexml.sourceforge.net
  * The JVoiceXML group hereby disclaims all copyright interest in the
  * library `JVoiceXML' (a free VoiceXML implementation).
  * JVoiceXML group, $Date$, Dirk Schnelle-Walka, project lead
@@ -54,6 +54,8 @@ public final class TestNodeHelper {
         speak.addText("1");
         speak.addText("  2 3  \t ");
         speak.addText("\r\n4 ");
+        speak.addText("5");
+        speak.addText("6");
         Assert.assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>"
                 + "<speak "
                 + "xmlns=\"http://www.w3.org/2001/10/synthesis\" "
@@ -63,7 +65,7 @@ public final class TestNodeHelper {
                         Locale.getDefault()) + "\" "
                 + "xsi:schemaLocation=\"http://www.w3.org/2001/10/synthesis "
                 + "http://www.w3.org/TR/speech-synthesis/synthesis.xsd\">"
-                + "1 2 3 4</speak>", document.toString());
+                + "1 2 3 4 5 6</speak>", document.toString());
     }
 
 }
