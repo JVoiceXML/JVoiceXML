99086d5c0e6f2b57d51e916531b731409acf4e06
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2014-05-06 08:37:12 +0000

    added unit tests to test loading of script refereneces with relative URI

--- org.jvoicexml/unittests/src/org/jvoicexml/interpreter/tagstrategy/TestScriptStrategy.java
+++ org.jvoicexml/unittests/src/org/jvoicexml/interpreter/tagstrategy/TestScriptStrategy.java
@@ -6,7 +6,7 @@
  *
  * JVoiceXML - A free VoiceXML implementation.
  *
- * Copyright (C) 2007-2013 JVoiceXML group - http://jvoicexml.sourceforge.net
+ * Copyright (C) 2007-2014 JVoiceXML group - http://jvoicexml.sourceforge.net
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Library General Public
@@ -25,16 +25,19 @@
  */
 package org.jvoicexml.interpreter.tagstrategy;
 
+import java.io.File;
 import java.net.URI;
+import java.net.URL;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.jvoicexml.Application;
 import org.jvoicexml.documentserver.JVoiceXmlDocumentServer;
-import org.jvoicexml.documentserver.schemestrategy.DocumentMap;
 import org.jvoicexml.documentserver.schemestrategy.MappedDocumentStrategy;
 import org.jvoicexml.event.JVoiceXMLEvent;
 import org.jvoicexml.event.error.BadFetchError;
+import org.jvoicexml.interpreter.JVoiceXmlApplication;
 import org.jvoicexml.interpreter.ScriptingEngine;
 import org.jvoicexml.xml.vxml.Script;
 import org.jvoicexml.xml.vxml.VoiceXmlDocument;
@@ -68,12 +71,10 @@ public final class TestScriptStrategy
      */
     @Before
     public void setUp() throws Exception {
-        final DocumentMap map = DocumentMap.getInstance();
         final JVoiceXmlDocumentServer server = new JVoiceXmlDocumentServer();
         server.addSchemeStrategy(new MappedDocumentStrategy());
-
-        uri = map.getUri("/script");
-        map.addDocument(uri, SCRIPT);
+        final URL url = TestScriptStrategy.class.getResource("factorial.js");
+        uri = url.toURI();
     }
 
     /**
@@ -120,6 +121,32 @@ public final class TestScriptStrategy
         Assert.assertEquals(24.0, scripting.eval("factorial(4);"));
     }
 
+    /**
+     * Test method for {@link org.jvoicexml.interpreter.tagstrategy.ScriptStrategy#execute(org.jvoicexml.interpreter.VoiceXmlInterpreterContext, org.jvoicexml.interpreter.VoiceXmlInterpreter, org.jvoicexml.interpreter.FormInterpretationAlgorithm, org.jvoicexml.interpreter.FormItem, org.jvoicexml.xml.VoiceXmlNode)}.
+     * @throws JVoiceXMLEvent
+     *         Test failed.
+     * @exception Exception
+     *            test failed
+     */
+    @Test
+    public void testExecuteRelativeSrc() throws JVoiceXMLEvent, Exception {
+        final VoiceXmlDocument doc = createDocument();
+        final Vxml vxml = doc.getVxml();
+        final Script script = vxml.appendChild(Script.class);
+        script.setSrc("org/jvoicexml/interpreter/tagstrategy/factorial.js");
+
+        final ScriptStrategy strategy = new ScriptStrategy();
+        final Application application = new JVoiceXmlApplication(null);
+        final File file = new File("unittests/src/test.vxml");
+        application.addDocument(file.toURI(), doc);
+        getContext().setApplication(application);
+        executeTagStrategy(script, strategy);
+
+        final ScriptingEngine scripting = getScriptingEngine();
+        Assert.assertEquals(42, scripting.getVariable("a"));
+        Assert.assertEquals(24.0, scripting.eval("factorial(4);"));
+    }
+    
     /**
      * Test method for {@link org.jvoicexml.interpreter.tagstrategy.ScriptStrategy#execute(org.jvoicexml.interpreter.VoiceXmlInterpreterContext, org.jvoicexml.interpreter.VoiceXmlInterpreter, org.jvoicexml.interpreter.FormInterpretationAlgorithm, org.jvoicexml.interpreter.FormItem, org.jvoicexml.xml.VoiceXmlNode)}.
      * @throws JVoiceXMLEvent
