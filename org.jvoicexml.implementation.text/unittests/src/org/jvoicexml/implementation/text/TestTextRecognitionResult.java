1ffbc1c5e9eb5eebdb48a3fdd74ca91c3ac54300
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2014-08-11 08:31:52 +0000

    bugfix: corrected evaluation of the semantic interpretation

--- org.jvoicexml.implementation.text/unittests/src/org/jvoicexml/implementation/text/TestTextRecognitionResult.java
+++ org.jvoicexml.implementation.text/unittests/src/org/jvoicexml/implementation/text/TestTextRecognitionResult.java
@@ -26,32 +26,56 @@
 
 package org.jvoicexml.implementation.text;
 
+import java.io.InputStream;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.jvoicexml.processor.srgs.GrammarChecker;
+import org.jvoicexml.processor.srgs.GrammarGraph;
+import org.jvoicexml.processor.srgs.SrgsXmlGrammarParser;
 import org.jvoicexml.xml.srgs.ModeType;
+import org.jvoicexml.xml.srgs.SrgsXmlDocument;
+import org.xml.sax.InputSource;
 
 /**
  * Test cases for {@link TextRecognitionResult}.
- *
+ * 
  * @author Dirk Schnelle-Walka
  * @version $Revision$
  * @since 0.6
  */
 public final class TestTextRecognitionResult {
-    
-    
-    /**Reference to the GrammarChecker object.*/
+    /** Reference to the GrammarChecker object. */
     private GrammarChecker grammarChecker;
 
     /**
-     * Test method for {@link org.jvoicexml.implementation.text.TextRecognitionResult#TextRecognitionResult(java.lang.String)}.
+     * Set up the test environment.
+     * @throws Exception
+     *         set up failed
+     * @since 0.7.7
+     */
+    @Before
+    public void setUp() throws Exception {
+        final SrgsXmlGrammarParser parser = new SrgsXmlGrammarParser();
+        final InputStream input = TestTextRecognitionResult.class
+                .getResourceAsStream("yesno.srgs");
+        final InputSource source = new InputSource(input);
+        final SrgsXmlDocument document = new SrgsXmlDocument(source);
+        final GrammarGraph graph = parser.parse(document);
+        grammarChecker = new GrammarChecker(null, graph);
+    }
+
+    /**
+     * Test method for
+     * {@link org.jvoicexml.implementation.text.TextRecognitionResult#TextRecognitionResult(java.lang.String)}
+     * .
      */
     @Test
     public void testTextRecognitionResult() {
         final String utterance = "test me";
-        final TextRecognitionResult result =
-            new TextRecognitionResult(utterance, grammarChecker);
+        final TextRecognitionResult result = new TextRecognitionResult(
+                utterance, grammarChecker);
         Assert.assertEquals(utterance, result.getUtterance());
         Assert.assertEquals(ModeType.VOICE, result.getMode());
         Assert.assertEquals(1.0f, result.getConfidence(), .001f);
@@ -59,26 +83,39 @@ public final class TestTextRecognitionResult {
     }
 
     /**
-     * Test method for {@link org.jvoicexml.implementation.text.TextRecognitionResult#TextRecognitionResult(java.lang.String)}.
+     * Test method for
+     * {@link org.jvoicexml.implementation.text.TextRecognitionResult#TextRecognitionResult(java.lang.String)}
+     * .
      */
     @Test
     public void testTextRecognitionResultNull() {
-        final TextRecognitionResult result =
-            new TextRecognitionResult(null, grammarChecker);
+        final TextRecognitionResult result = new TextRecognitionResult(null,
+                grammarChecker);
         Assert.assertNull(result.getUtterance());
         Assert.assertEquals(ModeType.VOICE, result.getMode());
         Assert.assertEquals(1.0f, result.getConfidence(), .001f);
         Assert.assertNull(result.getMark());
     }
 
+    @Test
+    public void testGetSemanticInterpretation() {
+        final String utterance = "yes";
+        final TextRecognitionResult result = new TextRecognitionResult(
+                utterance, grammarChecker);
+        Assert.assertTrue(result.isAccepted());
+        Assert.assertEquals("yes", result.getSemanticInterpretation());
+    }
+
     /**
-     * Test method for {@link org.jvoicexml.implementation.text.TextRecognitionResult#setMark(java.lang.String)}.
+     * Test method for
+     * {@link org.jvoicexml.implementation.text.TextRecognitionResult#setMark(java.lang.String)}
+     * .
      */
     @Test
     public void testSetMark() {
         final String utterance = "test me";
-        final TextRecognitionResult result =
-            new TextRecognitionResult(utterance, grammarChecker);
+        final TextRecognitionResult result = new TextRecognitionResult(
+                utterance, grammarChecker);
         Assert.assertNull(result.getMark());
 
         final String mark = "testmark";
