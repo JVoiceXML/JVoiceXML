7aad861eaf157939e855b96c587ab9f6cb164b0c
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2014-08-12 13:32:24 +0000

    using the mmi namespace prefix per default.

--- trunk/org.jvoicexml.mmi.events/src/org/jvoicexml/mmi/events/package-info.java
+++ trunk/org.jvoicexml.mmi.events/src/org/jvoicexml/mmi/events/package-info.java
@@ -1,4 +1,9 @@
 /**
  * MMI lifecycle events.
  */
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.w3.org/2008/04/mmi-arch", xmlns = { @XmlNs(prefix = "mmi", namespaceURI = "http://www.w3.org/2008/04/mmi-arch") }, elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package org.jvoicexml.mmi.events;
+
+import javax.xml.bind.annotation.XmlNs;
+
