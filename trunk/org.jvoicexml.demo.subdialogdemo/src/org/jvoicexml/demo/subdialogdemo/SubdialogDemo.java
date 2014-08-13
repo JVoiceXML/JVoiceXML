26515253717bcebc3f65368ae83bc1d2f4140976
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2014-08-13 16:21:13 +0000

    rewrote subdialog demo.

--- trunk/org.jvoicexml.demo.subdialogdemo/src/org/jvoicexml/demo/subdialogdemo/SubdialogDemo.java
+++ trunk/org.jvoicexml.demo.subdialogdemo/src/org/jvoicexml/demo/subdialogdemo/SubdialogDemo.java
@@ -6,7 +6,7 @@
  *
  * JVoiceXML Demo - Demo for the free VoiceXML implementation JVoiceXML
  *
- * Copyright (C) 2010-2012 JVoiceXML group - http://jvoicexml.sourceforge.net
+ * Copyright (C) 2010-2014 JVoiceXML group - http://jvoicexml.sourceforge.net
  * The JVoiceXML group hereby disclaims all copyright interest in the
  * library `JVoiceXML' (a free VoiceXML implementation).
  * JVoiceXML group, $Date$, Dirk Schnelle-Walka, project lead
@@ -63,13 +63,13 @@ public final class SubdialogDemo {
      */
     public static void main(final String[] args) {
         LOGGER.info("Starting 'subdialog' demo for JVoiceXML...");
-        LOGGER.info("(c) 2010-2012 by JVoiceXML group - "
+        LOGGER.info("(c) 2010-2014 by JVoiceXML group - "
                 + "http://jvoicexml.sourceforge.net/");
         final GenericClient client = new GenericClient();
         final File file = new File("subdialog.vxml");
         final URI subdialog = file.toURI();
         try {
-            Session session = client.call(subdialog, "jsapi10", "jsapi10",
+            Session session = client.call(subdialog, "jsapi20", "jsapi20",
                     "dummy");
             session.waitSessionEnd();
             session.hangup();
