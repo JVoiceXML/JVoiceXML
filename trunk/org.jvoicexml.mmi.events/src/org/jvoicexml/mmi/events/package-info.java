e27882c3b07214b1b3bbcccdea3601c4040569f8
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2014-08-13 11:22:21 +0000

    formatted.

--- trunk/org.jvoicexml.mmi.events/src/org/jvoicexml/mmi/events/package-info.java
+++ trunk/org.jvoicexml.mmi.events/src/org/jvoicexml/mmi/events/package-info.java
@@ -2,7 +2,10 @@
  * MMI lifecycle events.
  */
 
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.w3.org/2008/04/mmi-arch", xmlns = { @XmlNs(prefix = "mmi", namespaceURI = "http://www.w3.org/2008/04/mmi-arch") }, elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+@javax.xml.bind.annotation.XmlSchema(elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED,
+    namespace = "http://www.w3.org/2008/04/mmi-arch",
+    xmlns = { @XmlNs(prefix = "mmi", namespaceURI = "http://www.w3.org/2008/04/mmi-arch"),
+        @XmlNs(prefix = "", namespaceURI = "http://www.w3.org/2008/04/mmi-arch")})
 package org.jvoicexml.mmi.events;
 
 import javax.xml.bind.annotation.XmlNs;
