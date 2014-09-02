323faa14ce09538e4aa85d24fffe2686fcc6f426
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2014-09-02 07:15:55 +0000

    fixed project setup

--- org.jvoicexml.demo.objecttagdemo/src/org/jvoicexml/demo/objecttagdemo/ObjectTagDemo.java
+++ org.jvoicexml.demo.objecttagdemo/src/org/jvoicexml/demo/objecttagdemo/ObjectTagDemo.java
@@ -6,7 +6,7 @@
  *
  * JVoiceXML Demo - Demo for the free VoiceXML implementation JVoiceXML
  *
- * Copyright (C) 2005-2013 JVoiceXML group - http://jvoicexml.sourceforge.net
+ * Copyright (C) 2005-2014 JVoiceXML group - http://jvoicexml.sourceforge.net
  * The JVoiceXML group hereby disclaims all copyright interest in the
  * library `JVoiceXML' (a free VoiceXML implementation).
  * JVoiceXML group, $Date$, Dirk Schnelle-Walka, project lead
@@ -87,7 +87,7 @@ public final class ObjectTagDemo {
         }
 
         final ConnectionInformation client =
-            new BasicConnectionInformation("dummy", "jsapi10", "jsapi10");
+            new BasicConnectionInformation("dummy", "jsapi20", "jsapi20");
         final Session session = jvxml.createSession(client);
 
         session.call(uri);
@@ -101,7 +101,7 @@ public final class ObjectTagDemo {
      */
     public static void main(final String[] args) {
         LOGGER.info("Starting 'hello world' demo for JVoiceXML...");
-        LOGGER.info("(c) 2005-2013 by JVoiceXML group - "
+        LOGGER.info("(c) 2005-2014 by JVoiceXML group - "
                 + "http://jvoicexml.sourceforge.net/");
 
         try {
