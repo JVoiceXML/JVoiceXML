90f84a2fb9a1072c78b93e948731919faaf0efdb
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2014-10-10 18:11:10 +0000

    started to work on a simple input demo.

---
+++ org.jvoicexml.demo.simpleinputdemo/src/org/jvoicexml/demo/simpleinputdemo/SimpleInputDemo.java
@@ -0,0 +1,92 @@
+/*
+ * File:    $HeadURL: https://svn.code.sf.net/p/jvoicexml/code/trunk/org.jvoicexml.demo.helloworlddemo/src/org/jvoicexml/demo/helloworlddemo/HelloWorldDemo.java $
+ * Version: $LastChangedRevision: 4230 $
+ * Date:    $Date: 2014-09-02 09:02:50 +0200 (Tue, 02 Sep 2014) $
+ * Author:  $LastChangedBy: schnelle $
+ *
+ * JVoiceXML Demo - Demo for the free VoiceXML implementation JVoiceXML
+ *
+ * Copyright (C) 2005-2014 JVoiceXML group - http://jvoicexml.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+package org.jvoicexml.demo.simpleinputdemo;
+
+import java.io.File;
+import java.net.URI;
+
+import javax.naming.NamingException;
+
+import org.apache.log4j.Logger;
+import org.jvoicexml.Session;
+import org.jvoicexml.client.GenericClient;
+import org.jvoicexml.client.UnsupportedResourceIdentifierException;
+import org.jvoicexml.event.ErrorEvent;
+import org.jvoicexml.event.error.NoresourceError;
+
+/**
+ * Simple input demo.
+ * <p>
+ * Must be run with the system property
+ * <code>-Djava.security.policy=${config}/jvoicexml.policy</code> and
+ * the <code>config</code> folder added to the classpath.
+ * </p>
+ * <p>
+ * This demo requires that JVoiceXML is configured with the jsapi20
+ * implementation platform.
+ * </p>
+ * 
+ * @author Dirk Schnelle-Walka
+ * @version $Revision: 4230 $
+ */
+public final class SimpleInputDemo {
+    /** Logger for this class. */
+    private static final Logger LOGGER =
+            Logger.getLogger(SimpleInputDemo.class);
+
+    /**
+     * The main method.
+     * @param args Command line arguments. None expected.
+     */
+    public static void main(final String[] args) {
+        LOGGER.info("Starting 'simple input' demo for JVoiceXML...");
+        LOGGER.info("(c) 2014 by JVoiceXML group - "
+                + "http://jvoicexml.sourceforge.net/");
+
+        final GenericClient client = new GenericClient();
+        final File file = new File("simpleexample.vxml");
+        final URI dialog = file.toURI();
+        try {
+            Session session = client.call(dialog, "jsapi20", "jsapi20",
+                    "dummy");
+            session.waitSessionEnd();
+            session.hangup();
+        } catch (NamingException e) {
+            LOGGER.fatal(e.getMessage(), e);
+        } catch (NoresourceError e) {
+            LOGGER.info("do you have the jsapi20 implementation platform"
+                    + " configured?");
+            LOGGER.fatal(e.getMessage(), e);
+        } catch (ErrorEvent e) {
+            LOGGER.fatal(e.getMessage(), e);
+        } catch (UnsupportedResourceIdentifierException e) {
+            LOGGER.fatal(e.getMessage(), e);
+        } finally {
+            client.close();
+        }
+    }
+}
