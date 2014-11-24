3f025e0f7bc0e5c029289983c66e9c62275c9ecc
schnelle <schnelle@33f829fc-28d5-4ed2-817f-66d5dc451e06>
2014-11-24 14:32:42 +0000

    Fixed compilation problems

--- trunk/org.jvoicexml.implementation.jsapi10/src/org/jvoicexml/implementation/jsapi10/Jsapi10RecognitionResult.java
+++ trunk/org.jvoicexml.implementation.jsapi10/src/org/jvoicexml/implementation/jsapi10/Jsapi10RecognitionResult.java
@@ -24,10 +24,7 @@
  *
  */
 
-
 package org.jvoicexml.implementation.jsapi10;
-
-import java.util.Collection;
 
 import javax.speech.recognition.FinalRuleResult;
 import javax.speech.recognition.Result;
@@ -35,9 +32,9 @@ import javax.speech.recognition.ResultToken;
 
 import org.apache.log4j.Logger;
 import org.jvoicexml.RecognitionResult;
+import org.jvoicexml.event.error.SemanticError;
+import org.jvoicexml.interpreter.datamodel.DataModel;
 import org.jvoicexml.xml.srgs.ModeType;
-import org.mozilla.javascript.Context;
-import org.mozilla.javascript.Scriptable;
 
 /**
  * JSAPI 1.0 implementation of the result of the recognition process.
@@ -45,11 +42,10 @@ import org.mozilla.javascript.Scriptable;
  * @author Dirk Schnelle-Walka
  * @version $Revision$
  */
-public final class Jsapi10RecognitionResult
-        implements RecognitionResult {
+public final class Jsapi10RecognitionResult implements RecognitionResult {
     /** Logger for this class. */
-    private static final Logger LOGGER =
-        Logger.getLogger(Jsapi10RecognitionResult.class);
+    private static final Logger LOGGER = Logger
+            .getLogger(Jsapi10RecognitionResult.class);
 
     /** The semantic interpretation of the utterance. */
     private Object interpretation;
@@ -62,7 +58,9 @@ public final class Jsapi10RecognitionResult
 
     /**
      * Constructs a new object.
-     * @param res The result returned by the recognizer.
+     * 
+     * @param res
+     *            The result returned by the recognizer.
      */
     public Jsapi10RecognitionResult(final Result res) {
         result = res;
@@ -172,72 +170,52 @@ public final class Jsapi10RecognitionResult
      * {@inheritDoc}
      */
     @Override
-    public Object getSemanticInterpretation() {
+    public Object getSemanticInterpretation(final DataModel model)
+            throws SemanticError {
         if (interpretation == null) {
-            if (LOGGER.isDebugEnabled()) {
-                LOGGER.debug("creating semantic interpretation...");
-            }
-            final FinalRuleResult res = (FinalRuleResult) result;
-            final String[] tags = res.getTags();
-            if ((tags == null) || (tags.length == 0)) {
+            final FinalRuleResult finalResult = (FinalRuleResult) result;
+            final Object[] objecttags = finalResult.getTags();
+            if ((objecttags == null) || (objecttags.length == 0)) {
                 return null;
             }
-            final Context context = Context.enter();
-            context.setLanguageVersion(Context.VERSION_1_6);
-            // create a initial scope, do NOT allow access to all java objects
-            // check later if sealed initial scope should be used.
-            final Scriptable scope = context.initStandardObjects();
-            context.evaluateString(scope, "var out = new Object();", "expr", 1,
-                    null);
-            final Collection<String> props = new java.util.ArrayList<String>();
+            final String[] tags = toString(objecttags);
+            model.createVariable("out");
             for (String tag : tags) {
-                final String[] pair = tag.split("=");
-                // For Talking Java the '=' sign must be escaped. If so:
-                // remove it from the tag.
-                if (pair[0].endsWith("\\")) {
-                    pair[0] = pair[0].substring(0, pair[0].length() - 1);
-                }
-                final String source;
-                if (pair.length < 2) {
-                    source = "out = " + pair[0] + ";";
+                if (tag.trim().endsWith(";")) {
+                    model.evaluateExpression(tag, Object.class);
                 } else {
-                    final String[] nestedctx = pair[0].split("\\.");
-                    String seq = "";
-                    for (String part : nestedctx) {
-                        if (!seq.equals(pair[0])) {
-                            if (!seq.isEmpty()) {
-                                seq += ".";
-                            }
-                            seq += part;
-                            if (!props.contains("out." + seq)) {
-                                final String expr = "out." + seq
-                                    + " = new Object();";
-                                props.add("out." + seq);
-                                if (LOGGER.isDebugEnabled()) {
-                                    LOGGER.debug("setting: '" + expr + "'");
-                                }
-                                context.evaluateString(scope, expr, "expr", 1,
-                                        null);
-                            }
-                        }
-                    }
-                    source = "out." + pair[0] + " = " + pair[1] + ";";
+                    model.updateVariable("out", tag);
                 }
-                if (LOGGER.isDebugEnabled()) {
-                    LOGGER.debug("setting: '" + source + "'");
-                }
-                context.evaluateString(scope, source, "expr", 1, null);
             }
-            interpretation = scope.get("out", scope);
-            if (LOGGER.isDebugEnabled()) {
-                LOGGER.debug("...created semantic interpretation");
-            }
+            interpretation = model.readVariable("out", Object.class);
+            final String log = model.toString(interpretation);
+            LOGGER.info("created semantic interpretation '" + log + "'");
         }
         return interpretation;
     }
 
     /**
+     * Converts given object tags into a string representation.
+     * 
+     * @param objecttags
+     *            the object tags to convert.
+     * @return tags as string
+     * @since 0.7.7
+     */
+    private String[] toString(final Object[] objecttags) {
+        final String[] tags = new String[objecttags.length];
+        for (int i = 0; i < objecttags.length; i++) {
+            final Object o = objecttags[i];
+            if (o != null) {
+                tags[i] = o.toString();
+            }
+        }
+        return tags;
+    }
+
+    /**
      * {@inheritDoc}
+     * 
      * @since 0.7.3
      */
     @Override
@@ -247,7 +225,7 @@ public final class Jsapi10RecognitionResult
         str.append('[');
         str.append(getUtterance());
         str.append(',');
-        str.append(getSemanticInterpretation());
+        str.append(interpretation);
         str.append(',');
         str.append(isAccepted());
         str.append(',');
