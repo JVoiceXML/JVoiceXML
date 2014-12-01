9d067bcf09f45cc1ccbdf2ddf4045aa5fbe28345
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2014-12-01 08:46:14 +0000

    fixed unit tests.

--- org.jvoicexml/src/org/jvoicexml/interpreter/datamodel/KeyValuePair.java
+++ org.jvoicexml/src/org/jvoicexml/interpreter/datamodel/KeyValuePair.java
@@ -69,4 +69,57 @@ public class KeyValuePair {
     public Object getValue() {
         return value;
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((key == null) ? 0 : key.hashCode());
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        KeyValuePair other = (KeyValuePair) obj;
+        if (key == null) {
+            if (other.key != null) {
+                return false;
+            }
+        } else if (!key.equals(other.key)) {
+            return false;
+        }
+        if (value == null) {
+            if (other.value != null) {
+                return false;
+            }
+        } else if (!value.equals(other.value)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return getClass().getCanonicalName() + "[key=" + key + ", value="
+                + value + "]";
+    }
 }
