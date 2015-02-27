cdb4e08cd1e672db7ff0219385da61f0d22bea08
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2015-02-27 08:08:04 +0000

    Added lightweigtbml as a predefined connection information.

--- org.jvoicexml.client/src/org/jvoicexml/client/JVoiceXmlConnectionInformationFactory.java
+++ org.jvoicexml.client/src/org/jvoicexml/client/JVoiceXmlConnectionInformationFactory.java
@@ -6,7 +6,7 @@
  *
  * JVoiceXML - A free VoiceXML implementation.
  *
- * Copyright (C) 2012 JVoiceXML group - http://jvoicexml.sourceforge.net
+ * Copyright (C) 2012-2015 JVoiceXML group - http://jvoicexml.sourceforge.net
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -91,7 +91,7 @@ public final class JVoiceXmlConnectionInformationFactory
      */
     @Override
     public String[] getSystemOutputIdentifiers() {
-        return new String[] {"jsapi10", "jsapi20", "mary", "marc"};
+        return new String[] {"jsapi10", "jsapi20", "mary", "marc", "bml"};
     }
 
     /**
