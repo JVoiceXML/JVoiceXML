1593ff61417884b5dbab6688b7fc4be875689070
schnelle <dirk.schnelle@jvoicexml.org>
2017-01-23 17:43:52 +0100

    turned org.jvoicexml.implementation.mrcpv2 into a gradle project

---
+++ org.jvoicexml.implementation.mrcpv2/src/main/java/org/jvoicexml/callmanager/mrcpv2/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains SIP
+ * {@link org.jvoicexml.CallManager}.
+ */
+
+package org.jvoicexml.callmanager.mrcpv2;
+
---
+++ org.jvoicexml.implementation.mrcpv2/src/main/java/org/jvoicexml/implementation/mrcpv2/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * An implementation platform for MRCPv2.
+ *
+ * <p>
+ * The implementation is based on the
+ * <a href="http://cairo.sourceforge.net">cairo</a> project.
+ * </p>
+ */
+
+package org.jvoicexml.implementation.mrcpv2;
