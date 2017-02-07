3a127f18ca6330f9c2a79a83c3c5d803bcd41a24
schnelle <dirk.schnelle@jvoicexml.org>
2017-02-07 08:58:41 +0100

    converted script demo into a gradle project

--- org.jvoicexml.demo.scriptdemo/src/main/java/org/jvoicexml/demo/scriptdemo/ScriptDemo.java
+++ org.jvoicexml.demo.scriptdemo/src/main/java/org/jvoicexml/demo/scriptdemo/ScriptDemo.java
@@ -1,12 +1,7 @@
 /*
- * File:    $HeadURL$
- * Version: $LastChangedRevision$
- * Date:    $Date$
- * Author:  $LastChangedBy$
- *
  * JVoiceXML Demo - Demo for the free VoiceXML implementation JVoiceXML
  *
- * Copyright (C) 2005-2014 JVoiceXML group - http://jvoicexml.sourceforge.net
+ * Copyright (C) 2005-2017 JVoiceXML group - http://jvoicexml.sourceforge.net
  * The JVoiceXML group hereby disclaims all copyright interest in the
  * library `JVoiceXML' (a free VoiceXML implementation).
  * JVoiceXML group, $Date$, Dirk Schnelle-Walka, project lead
@@ -56,7 +51,6 @@ import org.jvoicexml.event.JVoiceXMLEvent;
  *
  * @author Torben Hardt
  * @author Dirk Schnelle-Walka
- * @version $Revision$
  */
 public final class ScriptDemo {
     /** Logger for this class. */
@@ -121,7 +115,7 @@ public final class ScriptDemo {
      */
     public static void main(final String[] args) {
         LOGGER.info("Starting sripting demo for JVoiceXML...");
-        LOGGER.info("(c) 2005-2015 by JVoiceXML group - "
+        LOGGER.info("(c) 2005-2017 by JVoiceXML group - "
                 + "http://jvoicexml.sourceforge.net/");
         try {
             final ScriptDemo demo = new ScriptDemo();
