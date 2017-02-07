de2be4eb2845e0ef0184228943d241748c84022b
schnelle <dirk.schnelle@jvoicexml.org>
2017-02-07 08:34:49 +0100

    converted record demo into a gradle project

--- org.jvoicexml.demo.recorddemo/src/main/java/org/jvoicexml/demo/recorddemo/RecordDemo.java
+++ org.jvoicexml.demo.recorddemo/src/main/java/org/jvoicexml/demo/recorddemo/RecordDemo.java
@@ -1,12 +1,7 @@
 /*
- * File:    $HeadURL$
- * Version: $LastChangedRevision$
- * Date:    $Date$
- * Author:  $LastChangedBy$
- *
  * JVoiceXML - A free VoiceXML implementation.
  *
- * Copyright (C) 2008-2014 JVoiceXML group - http://jvoicexml.sourceforge.net
+ * Copyright (C) 2008-2017 JVoiceXML group - http://jvoicexml.sourceforge.net
  * The JVoiceXML group hereby disclaims all copyright interest in the
  * library `JVoiceXML' (a free VoiceXML implementation).
  * JVoiceXML group, $Date$, Dirk Schnelle-Walka, project lead
@@ -54,7 +49,6 @@ import org.jvoicexml.event.error.NoresourceError;
  * </p>
  * 
  * @author Dirk Schnelle-Walka
- * @version $Revision$
  * @since 0.6
  */
 public final class RecordDemo {
@@ -75,7 +69,7 @@ public final class RecordDemo {
 	 */
 	public static void main(final String[] args) {
 		LOGGER.info("Starting 'record' demo for JVoiceXML...");
-		LOGGER.info("(c) 2008-2014 by JVoiceXML group - "
+		LOGGER.info("(c) 2008-2017 by JVoiceXML group - "
 				+ "http://jvoicexml.sourceforge.net/");
 
 		final GenericClient client = new GenericClient();
