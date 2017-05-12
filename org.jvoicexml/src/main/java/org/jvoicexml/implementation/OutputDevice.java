fbc2befcff4f668d38f46629356cc64365554809
schnelle <dirk.schnelle@jvoicexml.org>
2017-05-12 10:05:55 +0200

    converted howtobuild into a gradle project

--- org.jvoicexml/src/main/java/org/jvoicexml/implementation/OutputDevice.java
+++ org.jvoicexml/src/main/java/org/jvoicexml/implementation/OutputDevice.java
@@ -1,12 +1,7 @@
 /*
- * File:    $HeadURL$
- * Version: $LastChangedRevision$
- * Date:    $Date$
- * Author:  $LastChangedBy$
- *
  * JVoiceXML - A free VoiceXML implementation.
  *
- * Copyright (C) 2005-2013 JVoiceXML group - http://jvoicexml.sourceforge.net
+ * Copyright (C) 2005-2017 JVoiceXML group - http://jvoicexml.sourceforge.net
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Library General Public
@@ -33,7 +28,6 @@ import org.jvoicexml.xml.vxml.BargeInType;
  * An output device for synthesized or audio file output.
  *
  * @author Dirk Schnelle-Walka
- * @version $Revision$
  * @since 0.6
  */
 public interface OutputDevice {
