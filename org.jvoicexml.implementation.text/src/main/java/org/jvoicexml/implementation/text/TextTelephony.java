b5675cdb6bd83bf0da0d0b421dd0c58546a5df37
schnelle <dirk.schnelle@jvoicexml.org>
2018-11-13 12:20:59 +0100

    issue #56 Improved communication synchronization.

--- org.jvoicexml.implementation.text/src/main/java/org/jvoicexml/implementation/text/TextTelephony.java
+++ org.jvoicexml.implementation.text/src/main/java/org/jvoicexml/implementation/text/TextTelephony.java
@@ -1,7 +1,7 @@
 /*
  * JVoiceXML - A free VoiceXML implementation.
  *
- * Copyright (C) 2008-2015 JVoiceXML group - http://jvoicexml.sourceforge.net
+ * Copyright (C) 2008-2018 JVoiceXML group - http://jvoicexml.sourceforge.net
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
