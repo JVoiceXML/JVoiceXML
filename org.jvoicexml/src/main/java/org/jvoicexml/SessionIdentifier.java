9e9d84567f4a9dcd7eccbaaef9af81cab2ca133e
Dirk Schnelle-Walka <dirk.schnelle@jvoicexml.org>
2021-09-29 21:07:28 +0200

    #129 added equals and hashcode methods

--- org.jvoicexml/src/main/java/org/jvoicexml/SessionIdentifier.java
+++ org.jvoicexml/src/main/java/org/jvoicexml/SessionIdentifier.java
@@ -32,7 +32,9 @@ import java.io.Serializable;
  * 
  * Note, that it may be helpful to also override the {@code #toString()} method
  * to have something meaningful in log statements.
- *  
+ * 
+ * Implemnting classes MUST override {@link #equals(Object)} and
+ * {@link #hashCode()}.
  * @author Dirk Schnelle-Walka
  * @since 0.7.9
  */
